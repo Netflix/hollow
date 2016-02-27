40ee950d1930630fa0e23ed398d78cad18f94036
Rohit Kaul <rkaul@netflix.com>
2016-02-26 16:57:16 -0800

    no need for objectMapper.addObject(..)

--- src/main/java/com/netflix/vms/transformer/SimpleTransformer.java
+++ src/main/java/com/netflix/vms/transformer/SimpleTransformer.java
@@ -9,7 +9,6 @@ import com.netflix.vms.transformer.hollowinput.VideoDisplaySetHollow;
 import com.netflix.vms.transformer.hollowoutput.CompleteVideo;
 import com.netflix.vms.transformer.hollowoutput.CompleteVideoFacetData;
 import com.netflix.vms.transformer.hollowoutput.DeploymentIntent;
-import com.netflix.vms.transformer.hollowoutput.EncodingProfileGroup;
 import com.netflix.vms.transformer.hollowoutput.ISOCountry;
 import com.netflix.vms.transformer.hollowoutput.Video;
 import com.netflix.vms.transformer.hollowoutput.VideoCollectionsData;
@@ -81,7 +80,6 @@ public class SimpleTransformer {
         }
 
         objectMapper.addObject(new DeploymentIntent());
-        objectMapper.addObject(new EncodingProfileGroup());
 
         // Register Transform Modules
         List<TransformModule> moduleList = Arrays.<TransformModule>asList(
