5f2532cd0f92158232ccb1b1954bd5abf092210f
Drew Koszewnik <dkoszewnik@netflix.com>
2016-04-12 15:02:40 -0700

    Making this a multi-module project, containing both a transformer, which houses the business logic, and a server, which contains the platform infrastructure integration

---
+++ server/src/main/java/com/netflix/vms/transformer/servlet/platform/StartServerLifecycle.java
@@ -0,0 +1,67 @@
+package com.netflix.vms.transformer.servlet.platform;
+
+import com.google.inject.servlet.ServletModule;
+import com.netflix.aws.file.FileStore;
+import com.netflix.governator.annotations.AutoBindSingleton;
+import com.netflix.karyon.spi.HealthCheckHandler;
+import com.netflix.server.base.NFFilter;
+import com.netflix.server.base.lifecycle.BaseServerLifecycleListener;
+import com.sun.jersey.guice.JerseyServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletContextEvent;
+
+
+@AutoBindSingleton
+public class StartServerLifecycle extends BaseServerLifecycleListener {
+
+    public static FileStore FILE_STORE;
+
+    public StartServerLifecycle() {
+        super("vmstransformer", "vmstransformer", "1.0");
+    }
+
+    @Override
+    protected void initialize(ServletContextEvent event) throws Exception {
+        FileStore fileStore = getInjector().getInstance(FileStore.class);
+        FILE_STORE = fileStore;
+    }
+
+    @Override
+    protected ServletModule getServletModule() {
+        return new JerseyServletModule() {
+            @Override
+            protected void configureServlets() {
+                // initialize NFFilter
+                Map<String, String> initParams = new HashMap<String, String>();
+                initParams.put("requestId.accept", "true");
+                initParams.put("requestId.require", "true");
+                filter("/*").through(NFFilter.class, initParams);
+
+                // This sets up Jersey to scan the "com.netflix" and "com.sun.jersey" packages
+                // and serve any found resources from the base path of "/REST/"
+                serve("/REST/*").with(GuiceContainer.class, createStandardServeParams());
+
+                // fix bug in Jersey-Guice integration exposed by child injectors
+                binder().bind(GuiceContainer.class).asEagerSingleton();
+
+                serve("/Status").with(StatusPage.class);
+                serve("/healthcheck").with(HealthCheckServlet.class);
+
+                // fix bug in Jersey-Guice integration exposed by child injectors
+                binder().bind(GuiceContainer.class).asEagerSingleton();
+            }
+        };
+    }
+
+
+    @Override
+    protected Class<? extends HealthCheckHandler> getHealthCheckHandler() {
+        return HealthCheckHandlerImpl.class;
+    }
+
+}
+
