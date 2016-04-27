e161823fca79c681fbd686e7a91610756b8cb8fd
Drew Koszewnik <dkoszewnik@netflix.com>
2016-04-26 17:24:43 -0700

    Fixed ShowHierarchy -- uses VideoGeneral, VideoType, and ShowSeasonEpisode instead of (no longer present) VideoDisplaySet.

--- transformer/src/main/java/com/netflix/vms/transformer/index/IndexSpec.java
+++ transformer/src/main/java/com/netflix/vms/transformer/index/IndexSpec.java
@@ -5,18 +5,17 @@ import static com.netflix.vms.transformer.index.IndexSpec.IndexType.PRIMARY_KEY;
 
 public enum IndexSpec {
 
-    SUPPLEMENTAL(PRIMARY_KEY, "Trailer", "movieId"),
+    SUPPLEMENTAL(PRIMARY_KEY, "Supplementals", "movieId"),
     VIDEO_RIGHTS(PRIMARY_KEY, "VideoRights", "movieId", "countryCode.value"),
     VIDEO_TYPE(PRIMARY_KEY, "VideoType", "videoId"),
     VIDEO_GENERAL(PRIMARY_KEY, "VideoGeneral", "videoId"),
     STORAGE_GROUPS(PRIMARY_KEY, "StorageGroups", "id.value"),
-    CDNS(PRIMARY_KEY, "Cdns", "id"),
+    CDNS(PRIMARY_KEY, "Cdn", "id"),
     PROTECTION_TYPES(PRIMARY_KEY, "ProtectionTypes", "id"),
-    STORIES_SYNOPSES(PRIMARY_KEY, "Stories_Synopses", "movieId"),
+    STORIES_SYNOPSES(PRIMARY_KEY, "StoriesSynopses", "movieId"),
     STREAM_PROFILE(PRIMARY_KEY, "StreamProfiles", "id"),
     STREAM_PROFILE_GROUP(PRIMARY_KEY, "StreamProfileGroups", "groupName.value"),
     DEPLOYABLE_PACKAGES(PRIMARY_KEY, "DeployablePackages", "packageId"),
-    BCP47_CODE(PRIMARY_KEY, "Bcp47Code", "bcp47Code.value"),
     VIDEO_AWARD(PRIMARY_KEY, "VideoAward", "videoId"),
     VMS_AWARD(PRIMARY_KEY, "VMSAward", "awardId"),
     CSM_REVIEW(PRIMARY_KEY, "CSMReview", "videoId"),
@@ -25,13 +24,14 @@ public enum IndexSpec {
     CONSOLIDATED_VIDEO_RATINGS(PRIMARY_KEY, "ConsolidatedVideoRatings", "videoId"),
     CONSOLIDATED_CERT_SYSTEMS(PRIMARY_KEY, "ConsolidatedCertificationSystems", "certificationSystemId"),
     CERT_SYSTEM_RATING(PRIMARY_KEY, "ConsolidatedCertSystemRating", "ratingId"),
-    PACKAGES(PRIMARY_KEY, "Packages", "packageId"),
+    PACKAGES(PRIMARY_KEY, "Package", "packageId"),
 
+    SHOW_SEASON_EPISODE(HASH, "ShowSeasonEpisode", "", "movieId"),
     VIDEO_DATE(HASH, "VideoDate", "window.element", "videoId", "window.element.countryCode.value"),
     PERSONS_BY_VIDEO_ID(HASH, "VideoPerson", "", "cast.element.videoId"),
     PERSON_ROLES_BY_VIDEO_ID(HASH, "VideoPerson", "cast.element", "personId", "cast.element.videoId"),
-    VIDEO_TYPE_COUNTRY(HASH, "VideoType", "type.element", "videoId", "type.element.countryCode.value"),
-    PACKAGES_BY_VIDEO(HASH, "Packages", "", "movieId"),
+    VIDEO_TYPE_COUNTRY(HASH, "VideoType", "countryInfos.element", "videoId", "countryInfos.element.countryCode.value"),
+    PACKAGES_BY_VIDEO(HASH, "Package", "", "movieId"),
     ALL_VIDEO_RIGHTS(HASH, "VideoRights", "", "movieId"),
     ROLLOUT_VIDEO_TYPE(HASH, "Rollout", "", "movieId", "rolloutType.value");
 
