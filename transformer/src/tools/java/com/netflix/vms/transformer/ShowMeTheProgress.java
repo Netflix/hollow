a57f178fa35432e0fcf7e1017fff614f07be0fa1
Rohit Kaul <rkaul@netflix.com>
2016-04-27 11:37:14 -0700

    fix compilation

--- transformer/src/tools/java/com/netflix/vms/transformer/ShowMeTheProgress.java
+++ transformer/src/tools/java/com/netflix/vms/transformer/ShowMeTheProgress.java
@@ -46,7 +46,7 @@ public class ShowMeTheProgress {
 
         filter.addTypeRecursive("PackageData", outputSchemas);
 
-        filter.addTypeRecursive("DrmSystem", outputSchemas);
+        // filter.addTypeRecursive("DrmSystem", outputSchemas);
         filter.addTypeRecursive("OriginServer", outputSchemas);
         filter.addTypeRecursive("ArtWorkImageFormatEntry", outputSchemas);
         filter.addTypeRecursive("EncodingProfile", outputSchemas);
