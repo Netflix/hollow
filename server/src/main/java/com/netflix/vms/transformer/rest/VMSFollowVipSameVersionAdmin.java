5d1af214dadbcf96c6a59a4f17f33d8b5c9dedef
dsu <dsu@netflix.com>
2016-06-27 10:50:57 -0700

    Refactor DataIO to use FollowVip

--- server/src/main/java/com/netflix/vms/transformer/rest/VMSFollowVipSameVersionAdmin.java
+++ server/src/main/java/com/netflix/vms/transformer/rest/VMSFollowVipSameVersionAdmin.java
@@ -1,7 +1,5 @@
 package com.netflix.vms.transformer.rest;
 
-import java.util.Collections;
-
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.netflix.aws.file.FileAccessItem;
@@ -9,10 +7,13 @@ import com.netflix.aws.file.FileStore;
 import com.netflix.vms.transformer.common.config.TransformerConfig;
 import com.netflix.vms.transformer.input.FileStoreUtil;
 import com.netflix.vms.transformer.util.HollowBlobKeybaseBuilder;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -23,86 +24,119 @@ import javax.ws.rs.core.Response;
 @Singleton
 @Path("/vms/followvipsameversion")
 public class VMSFollowVipSameVersionAdmin {
-    
-    
+
     private final FileStore fileStore;
     private final TransformerConfig config;
-    
+
     @Inject
     public VMSFollowVipSameVersionAdmin(FileStore fileStore, TransformerConfig config) {
         this.fileStore = fileStore;
         this.config = config;
     }
-    
+
     @GET
-    @Produces({MediaType.TEXT_PLAIN})
-    public Response findSameVersions(@QueryParam("fromVip") String fromVip, @QueryParam("toVip") String toVip) throws Exception {
-        
+    @Produces({ MediaType.TEXT_PLAIN, MediaType.TEXT_HTML, MediaType.APPLICATION_JSON })
+    public Response findSameVersions(@QueryParam("fromVip") String fromVip, @QueryParam("toVip") String toVip, @QueryParam("format") String format) throws Exception {
         if(fromVip == null)
-            fromVip = config.getTransformerVip();
+            fromVip = config.getFollowVip();
         if(toVip == null)
-            toVip = config.getFollowVip();
-        
-        
+            toVip = config.getTransformerVip();
+
         Map<String, Long>fromVipVersions = getVersionsByInputParameters(fromVip);
         Map<String, Long>toVipVersions = getVersionsByInputParameters(toVip);
-        
+
         List<VersionPair> versionPairs = new ArrayList<VersionPair>();
-        
+
         for(Map.Entry<String, Long> entry : fromVipVersions.entrySet()) {
             Long fromVipVersion = entry.getValue();
             Long toVipVersion = toVipVersions.get(entry.getKey());
-            
+
             if(toVipVersion != null)
                 versionPairs.add(new VersionPair(fromVipVersion, toVipVersion));
         }
-        
+
         Collections.sort(versionPairs);
-        
-        StringBuilder response = new StringBuilder();
-        
-        for(int i=0;i<versionPairs.size();i++) {
-            response.append(versionPairs.get(i).getFromVersion()).append("=").append(versionPairs.get(i).getToVersion()).append("\n");
+
+        String openResponse = "";
+        String dataFormat = "%s:%s=%s:%s";
+        String lineBreak = "\n";
+        String closeResponse = "";
+
+        boolean isHTML = false;
+        String mediaType = MediaType.TEXT_PLAIN;
+        if ("json".equals(format)) {
+            openResponse = "[";
+            dataFormat = "{\"%s\":%s, \"%s\":%s}";
+            closeResponse = "]";
+            lineBreak = ",\n";
+            mediaType = MediaType.APPLICATION_JSON;
+        } else if ("html".equals(format)) {
+            isHTML = true;
+            openResponse = "<html><head></head><body>";
+            dataFormat = "[%s:%s, %s:%s] - %s";
+            closeResponse = "</body><html>";
+            lineBreak = "<br>";
+            mediaType = MediaType.TEXT_HTML;
         }
-        
-        return Response.ok(response.toString(), MediaType.TEXT_PLAIN).build();
+
+
+        StringBuilder response = new StringBuilder(openResponse);
+        for(int i=0;i<versionPairs.size();i++) {
+            if (i > 0) response.append(lineBreak);
+
+            long fromVer = versionPairs.get(i).getFromVersion();
+            long toVer = versionPairs.get(i).getToVersion();
+            String dataEntry = isHTML ? String.format(dataFormat, fromVip, fromVer, toVip, toVer, createDiffLink(fromVip, fromVer, toVip, toVer)) : String.format(dataFormat, fromVip, fromVer, toVip, toVer);
+            response.append(dataEntry);
+        }
+        response.append(closeResponse);
+
+        return Response.ok(response.toString(), mediaType).build();
     }
 
     private final Map<String, Long> getVersionsByInputParameters(String vip) throws Exception {
         HollowBlobKeybaseBuilder keybaseBuilder = new HollowBlobKeybaseBuilder(vip);
 
         Map<String, Long> map = new HashMap<String, Long>();
-        
+
         addVersionsByKeybase(map, keybaseBuilder.getSnapshotKeybase());
         addVersionsByKeybase(map, keybaseBuilder.getDeltaKeybase());
-        
+
         return map;
     }
 
     private void addVersionsByKeybase(Map<String, Long> map, String snapshotKeybase) {
         List<FileAccessItem> allVersionItems = fileStore.getAllFileAccessItems(snapshotKeybase);
-        
+
         for(FileAccessItem item : allVersionItems) {
             Long toVersion = FileStoreUtil.getToVersion(item);
             Long inputVersion = FileStoreUtil.getInputDataVersion(item);
             Long cycleDataTimestamp = FileStoreUtil.getPublishCycleDataTS(item);
-            
+
             String key = inputVersion + "_" + cycleDataTimestamp;
-            
+
             map.put(key, toVersion);
         }
     }
-    
-    
+
+    private String createDiffLink(String primaryVip, long primaryCycleId, String secondaryVip, long secondaryCycleId) {
+        String env = config.getNetflixEnvironment();
+        String diffName = String.format("dataio:(%s:%s|%s:%s)", primaryVip, primaryCycleId, secondaryVip, secondaryCycleId);
+        String url = String.format("http://go/vmsdiff-%s?action=submit&diffName=%s&fromVip=%s&fromVersion=%s&toVip=%s&toVersion=%s",
+                env, diffName, primaryVip, primaryCycleId, secondaryVip, secondaryCycleId);
+
+        return String.format("[ <a href=\"%s\" target=\"_blank\">diff</a> ]", url);
+    }
+
     private static class VersionPair implements Comparable<VersionPair> {
         private final long fromVersion;
         private final long toVersion;
-        
+
         public VersionPair(long fromVersion, long toVersion) {
             this.fromVersion = fromVersion;
             this.toVersion = toVersion;
         }
-        
+
         public long getFromVersion() {
             return fromVersion;
         }
@@ -116,5 +150,5 @@ public class VMSFollowVipSameVersionAdmin {
             return Long.compare(other.fromVersion, fromVersion);
         }
     }
-    
+
 }
