30d0a30c66912024174fe4c54deac45cccda98cf
dsu <dsu@netflix.com>
2016-07-14 09:35:37 -0700

    Add constructor that takes proxyURL

--- server/src/tools/java/com/netflix/vms/transformer/testutil/repro/TransformerScenario.java
+++ server/src/tools/java/com/netflix/vms/transformer/testutil/repro/TransformerScenario.java
@@ -1,10 +1,4 @@
 package com.netflix.vms.transformer.testutil.repro;
-import java.io.InputStream;
-
-import java.util.Properties;
-import com.netflix.vms.transformer.util.HollowBlobKeybaseBuilder;
-import com.netflix.vms.transformer.http.HttpHelper;
-import com.netflix.vms.transformer.testutil.slice.DataSlicer;
 import com.netflix.hollow.read.engine.HollowBlobReader;
 import com.netflix.hollow.read.engine.HollowReadStateEngine;
 import com.netflix.hollow.util.HashCodes;
@@ -14,27 +8,33 @@ import com.netflix.vms.transformer.SimpleTransformer;
 import com.netflix.vms.transformer.SimpleTransformerContext;
 import com.netflix.vms.transformer.VMSTransformerWriteStateEngine;
 import com.netflix.vms.transformer.hollowinput.VMSHollowInputAPI;
+import com.netflix.vms.transformer.http.HttpHelper;
 import com.netflix.vms.transformer.input.VMSInputDataClient;
+import com.netflix.vms.transformer.testutil.slice.DataSlicer;
+import com.netflix.vms.transformer.util.HollowBlobKeybaseBuilder;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import net.jpountz.lz4.LZ4BlockInputStream;
 import net.jpountz.lz4.LZ4BlockOutputStream;
 
 public class TransformerScenario {
-    
+
     private final String localBlobStore;
     private final String transformerVip;
     private final long outputDataVersion;
     private final int[] topNodesToProcess;
-    
+
     private String proxyURL = VMSInputDataClient.PROD_PROXY_URL;
-    
+
     private String converterVip;
     private long inputDataVersion;
     private long processTimestamp;
-    
 
     public TransformerScenario(String localBlobStore, String transformerVip, long outputDataVersion, int... topNodesToProcess) {
         this.localBlobStore = localBlobStore;
@@ -42,80 +42,85 @@ public class TransformerScenario {
         this.outputDataVersion = outputDataVersion;
         this.topNodesToProcess = topNodesToProcess;
     }
-    
+
+    public TransformerScenario(String proxyURL, String localBlobStore, String transformerVip, long outputDataVersion, int... topNodesToProcess) {
+        this(localBlobStore, transformerVip, outputDataVersion, topNodesToProcess);
+        this.proxyURL = proxyURL;
+    }
+
     public VMSTransformerWriteStateEngine repro() throws Throwable {
         File scenarioInputFile = scenarioInputDataFile();
-        
+
         HollowReadStateEngine inputStateEngineSlice;
-        
+
         if(scenarioInputFile.exists()) {
             inputStateEngineSlice = readStateEngineSlice(scenarioInputFile);
         } else {
             inputStateEngineSlice = createStateEngineSlice(scenarioInputFile);
         }
-        
+
         VMSHollowInputAPI api = new VMSHollowInputAPI(inputStateEngineSlice);
         VMSTransformerWriteStateEngine outputStateEngine = new VMSTransformerWriteStateEngine();
         SimpleTransformerContext ctx = new SimpleTransformerContext();
         ctx.setNowMillis(processTimestamp);
         new SimpleTransformer(api, outputStateEngine, ctx).transform();
-        
+
         return outputStateEngine;
     }
 
     private HollowReadStateEngine createStateEngineSlice(File scenarioInputFile) throws IOException {
         determineInputParameters();
-        
+
         VMSInputDataClient client = new VMSInputDataClient(proxyURL, localBlobStore, converterVip);
-        
+
         client.triggerRefreshTo(inputDataVersion);
-        
+
         DataSlicer slicer = new DataSlicer(0, topNodesToProcess);
-        
+
         HollowWriteStateEngine slicedStateEngine = slicer.sliceInputBlob(client.getStateEngine());
-        
+
         slicedStateEngine.addHeaderTag("publishCycleDataTS", String.valueOf(processTimestamp));
-        
+
         writeStateEngineSlice(slicedStateEngine, scenarioInputFile);
 
         return readStateEngineSlice(scenarioInputFile);
     }
-    
+
     private void determineInputParameters() throws IOException {
         HollowBlobKeybaseBuilder keybaseBuilder = new HollowBlobKeybaseBuilder(transformerVip);
-        
+
         Properties dataProps = new Properties();
         InputStream is = HttpHelper.getInputStream(proxyURL + "/filestore-attribute?keybase=" + keybaseBuilder.getReverseDeltaKeybase() + "&version=" + outputDataVersion);
         dataProps.load(is);
-        
+
         this.converterVip = dataProps.getProperty("converterVip");
         this.inputDataVersion = Long.parseLong(dataProps.getProperty("inputVersion"));
         this.processTimestamp = Long.parseLong(dataProps.getProperty("publishCycleDataTS"));
     }
-    
+
     private File scenarioInputDataFile() {
         return new File(localBlobStore, "scenario-" + transformerVip + "-" + outputDataVersion + "-" + Integer.toHexString(inputVideosHashCode()));
     }
-    
+
     private int inputVideosHashCode() {
         int hashCode = 0;
         for(int i : topNodesToProcess)
             hashCode ^= HashCodes.hashInt(i);
         return hashCode;
     }
-    
+
     private HollowReadStateEngine readStateEngineSlice(File sliceFile) throws IOException {
         HollowReadStateEngine stateEngine = new HollowReadStateEngine();
         HollowBlobReader reader = new HollowBlobReader(stateEngine);
         try(LZ4BlockInputStream is = new LZ4BlockInputStream(new FileInputStream(sliceFile))) {
             reader.readSnapshot(is);
         }
-        
+
         this.processTimestamp = Long.parseLong(stateEngine.getHeaderTag("publishCycleDataTS"));
-        
+
         return stateEngine;
     }
-    
+
     private void writeStateEngineSlice(HollowWriteStateEngine slicedStateEngine, File sliceFile) throws IOException {
         HollowBlobWriter writer = new HollowBlobWriter(slicedStateEngine);
         try(LZ4BlockOutputStream os = new LZ4BlockOutputStream(new FileOutputStream(sliceFile))) {
