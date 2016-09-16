58f8ce2e48bcfac9ac8c7dec237805353ab1534e
Drew Koszewnik <dkoszewnik@netflix.com>
2016-09-16 16:56:36 -0700

    Added onHold flag to AvailabilityWindow output POJOs

--- business-logic/src/main/java/com/netflix/vms/transformer/hollowoutput/DateWindow.java
+++ business-logic/src/main/java/com/netflix/vms/transformer/hollowoutput/DateWindow.java
@@ -5,6 +5,7 @@ public class DateWindow implements Cloneable {
 
     public long startDateTimestamp = java.lang.Long.MIN_VALUE;
     public long endDateTimestamp = java.lang.Long.MIN_VALUE;
+    public boolean onHold = false;
 
     public boolean equals(Object other) {
         if(other == this)  return true;
@@ -14,6 +15,7 @@ public class DateWindow implements Cloneable {
         DateWindow o = (DateWindow) other;
         if(o.startDateTimestamp != startDateTimestamp) return false;
         if(o.endDateTimestamp != endDateTimestamp) return false;
+        if(onHold != o.onHold) return false;
         return true;
     }
 
@@ -21,6 +23,7 @@ public class DateWindow implements Cloneable {
         int hashCode = 1;
         hashCode = hashCode * 31 + (int) (startDateTimestamp ^ (startDateTimestamp >>> 32));
         hashCode = hashCode * 31 + (int) (endDateTimestamp ^ (endDateTimestamp >>> 32));
+        hashCode = hashCode * 31 + (onHold ? 1231 : 1237);
         return hashCode;
     }
 
@@ -28,6 +31,7 @@ public class DateWindow implements Cloneable {
         StringBuilder builder = new StringBuilder("DateWindow{");
         builder.append("startDateTimestamp=").append(startDateTimestamp);
         builder.append(",endDateTimestamp=").append(endDateTimestamp);
+        builder.append(",onHold=").append(onHold);
         builder.append("}");
         return builder.toString();
     }
