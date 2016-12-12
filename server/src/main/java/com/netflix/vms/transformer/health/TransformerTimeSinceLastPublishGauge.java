44b39776c75bdc5b64d1cccb94f261f011858179
Drew Koszewnik <dkoszewnik@netflix.com>
2016-12-12 12:14:51 -0800

    Adding timeSinceLastPublish metric

---
+++ server/src/main/java/com/netflix/vms/transformer/health/TransformerTimeSinceLastPublishGauge.java
@@ -0,0 +1,20 @@
+package com.netflix.vms.transformer.health;
+
+import static com.netflix.servo.annotations.DataSourceType.GAUGE;
+
+import com.netflix.servo.annotations.Monitor;
+
+public class TransformerTimeSinceLastPublishGauge {
+    
+    private long lastPublishTime = System.currentTimeMillis();
+    
+    public void notifyPublishSuccess() {
+        this.lastPublishTime = System.currentTimeMillis();
+    }
+    
+    @Monitor(name="timeSinceLastPublish", type=GAUGE)
+    public long getTimeSinceLastPublish() {
+        return System.currentTimeMillis() - lastPublishTime;
+    }
+
+}
