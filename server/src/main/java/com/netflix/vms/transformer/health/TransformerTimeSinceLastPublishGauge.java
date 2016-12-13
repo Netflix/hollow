a367ca3aaca5f715be6c596d4d90c0036889646a
Drew Koszewnik <dkoszewnik@netflix.com>
2016-12-12 16:57:49 -0800

    Minor update to TimeSinceLastPublish gauge name

--- server/src/main/java/com/netflix/vms/transformer/health/TransformerTimeSinceLastPublishGauge.java
+++ server/src/main/java/com/netflix/vms/transformer/health/TransformerTimeSinceLastPublishGauge.java
@@ -12,7 +12,7 @@ public class TransformerTimeSinceLastPublishGauge {
         this.lastPublishTime = System.currentTimeMillis();
     }
     
-    @Monitor(name="vms.transformer.timeSinceLastPublish", type=GAUGE)
+    @Monitor(name="vms.transformer.TimeSinceLastPublish", type=GAUGE)
     public long getTimeSinceLastPublish() {
         return System.currentTimeMillis() - lastPublishTime;
     }
