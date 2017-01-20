81fac44af1ea5da5a3bb1300fdde2083ef73fddb
OpsEng Jenkins Builds User <builds@netflix.com>
2017-01-20 23:40:23 +0000

    `gradlew` updated and locked by https://vms.builds.test.netflix.net/job/VMS-vmstransformer-update-gradlew/249/.

--- gradlew.bat
+++ gradlew.bat
@@ -8,17 +8,17 @@
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-
 @rem Netflix: Attempt to use $WORKSPACE/.gradle if we're not told otherwise
 if "%GRADLE_USER_HOME%"=="" (
     if NOT "%WORKSPACE%"=="" (
         set GRADLE_USER_HOME=%WORKSPACE%\.gradle
     )
 )
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
