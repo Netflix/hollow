fcccbd5a9f49bdd2ea40a058f909ce404c631197
dsu <dsu@netflix.com>
2017-02-02 11:29:08 -0800

    Alternative implentation of ShowMeTheProgress
    - Does not require pre-setup: RebuildTransformerSampleData that requires
    significant CPU and long duration
    
    - It takes advantage of Title Pinning Code and FastLane

--- business-logic/src/main/java/com/netflix/vms/transformer/util/OutputUtil.java
+++ business-logic/src/main/java/com/netflix/vms/transformer/util/OutputUtil.java
@@ -3,6 +3,7 @@ package com.netflix.vms.transformer.util;
 import com.netflix.vms.transformer.hollowinput.DateHollow;
 import com.netflix.vms.transformer.hollowoutput.Date;
 import com.netflix.vms.transformer.hollowoutput.Integer;
+import java.util.concurrent.TimeUnit;
 
 public class OutputUtil {
     public static final long FIVE_SECOND_MILLIS = 5000l;
@@ -33,5 +34,29 @@ public class OutputUtil {
 
     public static Date getRoundedDate(DateHollow dateHollow) {
         return dateHollow != null ? getRoundedDate(dateHollow._getValue()) : null;
+    }
+
+    public static String formatDuration(final long milliseconds, final boolean isShowActualValue) {
+        final String duration = formatAsElapsedTime(milliseconds, " ");
+        return isShowActualValue ? String.format("%d - (%s)", milliseconds, duration) : duration;
+    }
+
+    public static String formatAsElapsedTime(final long milliseconds, final String delim) {
+        long _millis = milliseconds;
+        final long _days = TimeUnit.MILLISECONDS.toDays(_millis);
+        _millis -= TimeUnit.DAYS.toMillis(_days);
+        final long _hours = TimeUnit.MILLISECONDS.toHours(_millis);
+        _millis -= TimeUnit.HOURS.toMillis(_hours);
+        final long _minutes = TimeUnit.MILLISECONDS.toMinutes(_millis);
+        _millis -= TimeUnit.MINUTES.toMillis(_minutes);
+        final long _seconds = TimeUnit.MILLISECONDS.toSeconds(_millis);
+
+        String format = "H:%02d%sM:%02d%sS:%02d";
+        if (_days > 0) {
+            format = "Days:%d " + format;
+            return String.format(format, _days, _hours, delim, _minutes, delim, _seconds);
+        }
+
+        return String.format(format, _hours, delim, _minutes, delim, _seconds);
     }
 }
