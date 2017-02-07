488a8f9b1ac2803cf8694de3a0de4ef8408efc86
Tim Taylor <tim@tool-man.org>
2017-02-07 14:12:42 -0800

    producer api: lift high level API from reference impl

---
+++ hollow/src/main/java/com/netflix/hollow/api/producer/HollowProducer.java
@@ -0,0 +1,128 @@
+/*
+ *
+ *  Copyright 2017 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.hollow.api.producer;
+
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.netflix.hollow.api.StateTransition;
+import com.netflix.hollow.core.read.engine.HollowReadStateEngine;
+import com.netflix.hollow.core.write.HollowBlobWriter;
+import com.netflix.hollow.core.write.HollowWriteStateEngine;
+
+public class HollowProducer {
+
+    private final VersionMinter versionMinter;
+    private final HollowPublisher publisher;
+    private final HollowAnnouncer announcer;
+    private final HollowWriteStateEngine writeEngine;
+
+    private StateTransition announced;
+
+    public HollowProducer(VersionMinter versionMinter, HollowPublisher publisher, HollowAnnouncer announcer) {
+        this.versionMinter = versionMinter;
+        this.publisher = publisher;
+        this.announcer = announcer;
+
+        writeEngine = new HollowWriteStateEngine();
+        announced = new StateTransition();
+    }
+
+    public void restoreFrom(HollowReadStateEngine priorAnnouncedState, long priorAnnouncedVersion) {
+        writeEngine.restoreFrom(priorAnnouncedState);
+        announced = new StateTransition(priorAnnouncedVersion);
+    }
+
+    /**
+     * Each cycle produces a single state.
+     */
+    public void produce(Task task) {
+        try {
+            WriteState writeState = beginCycle(announced.advance(versionMinter.mint()));
+            task.populate(writeState);
+            publish(writeState);
+            announced = announce(writeState);
+        } catch(Throwable th) {
+            th.printStackTrace();
+            rollback();
+        }
+    }
+
+    private WriteState beginCycle(StateTransition transition) {
+        writeEngine.prepareForNextCycle();
+        WriteState writeState = new WriteState(writeEngine, transition);
+        System.out.println("Beginning cycle " + transition);
+        return writeState;
+    }
+
+    private void publish(WriteState writeState) throws IOException {
+        HollowBlobWriter writer = new HollowBlobWriter(writeEngine);
+
+
+        File snapshotFile = publisher.openSnapshot(announced.getToVersion());
+        File deltaFile = publisher.openDelta(announced.getFromVersion(), announced.getToVersion());
+        File reverseDeltaFile = publisher.openReverseDelta(announced.getFromVersion(), announced.getToVersion());
+
+        try(OutputStream os = new BufferedOutputStream(new FileOutputStream(snapshotFile))) {
+            writer.writeSnapshot(os);
+        }
+
+        if(announced.isDelta()) {
+
+            try(OutputStream os = new BufferedOutputStream(new FileOutputStream(deltaFile))) {
+                writer.writeDelta(os);
+            }
+
+            try(OutputStream os = new BufferedOutputStream(new FileOutputStream(reverseDeltaFile))) {
+                writer.writeReverseDelta(os);
+            }
+
+            publisher.publishDelta(deltaFile, announced.getFromVersion(), announced.getToVersion());
+            publisher.publishReverseDelta(reverseDeltaFile, announced.getFromVersion(), announced.getToVersion());
+
+            deltaFile.delete();
+            reverseDeltaFile.delete();
+        }
+
+        try {
+            /// it's ok to fail to publish a snapshot, as long as you don't miss too many in a row.
+            /// you can add a timeout or even do this in a separate thread.
+            publisher.publishSnapshot(snapshotFile, announced.getToVersion());
+            snapshotFile.delete();
+        } catch(Throwable ignore) { }
+    }
+
+    private void rollback() {
+        writeEngine.resetToLastPrepareForNextCycle();
+    }
+
+    private StateTransition announce(WriteState writeState) {
+        announcer.announce(writeState.getVersion());
+        StateTransition transition = writeState.getTransition();
+        return transition;
+    }
+
+    public static interface Task {
+        void populate(WriteState newState);
+    }
+
+}
