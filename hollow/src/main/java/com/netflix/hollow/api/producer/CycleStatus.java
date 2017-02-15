93e27e8947eb79bf86d5f23c36b27a1f10595a40
Tim Taylor <tim@toolbear.io>
2017-02-14 20:38:44 -0800

    make listening to multiple producers easier to implement

--- hollow/src/main/java/com/netflix/hollow/api/producer/CycleStatus.java
+++ hollow/src/main/java/com/netflix/hollow/api/producer/CycleStatus.java
@@ -1,8 +1,27 @@
+/*
+ *
+ *  Copyright 2016 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
 package com.netflix.hollow.api.producer;
 
 import com.netflix.hollow.core.read.engine.HollowReadStateEngine;
 
 /**
+ * Beta API subject to change.
+ *
  * This class represents information on details when {@link HollowProducer} has finished executing a particular stage.
  * An instance of this class is provided on different events of {@link HollowProducerListener}.
  *
@@ -14,28 +33,39 @@ public class CycleStatus {
         SUCCESS, FAIL
     }
 
+    private final HollowProducer producer;
     private long version;
     private Status status;
     private Throwable throwable;
     private HollowReadStateEngine readStateEngine;
 
-    public static CycleStatus getSuccessInstance(long version) {
-        return new CycleStatus(version, Status.SUCCESS, null, null);
+    public static CycleStatus success(HollowProducer producer, long version) {
+        return new CycleStatus(producer, version, Status.SUCCESS, null, null);
     }
 
-    public static CycleStatus getSuccessInstance(long version, HollowReadStateEngine readStateEngine) {
-        return new CycleStatus(version, Status.SUCCESS, null, readStateEngine);
+    public static CycleStatus success(HollowProducer producer, long version, HollowReadStateEngine readStateEngine) {
+        return new CycleStatus(producer, version, Status.SUCCESS, null, readStateEngine);
     }
 
-    public static CycleStatus getFailInstance(long version, Throwable th) {
-        return new CycleStatus(version, Status.FAIL, th, null);
+    public static CycleStatus fail(HollowProducer producer, long version, Throwable th) {
+        return new CycleStatus(producer, version, Status.FAIL, th, null);
     }
 
-    CycleStatus(long version, Status status, Throwable throwable, HollowReadStateEngine readStateEngine) {
+    CycleStatus(HollowProducer producer, long version, Status status, Throwable throwable, HollowReadStateEngine readStateEngine) {
+        this.producer = producer;
         this.version = version;
         this.status = status;
         this.throwable = throwable;
         this.readStateEngine = readStateEngine;
+    }
+
+    /**
+     * The producer whose status is being described.
+     *
+     * @return the producer
+     */
+    public HollowProducer getProducer() {
+        return producer;
     }
 
     /**
--- hollow/src/main/java/com/netflix/hollow/api/producer/HollowProducerListener.java
+++ hollow/src/main/java/com/netflix/hollow/api/producer/HollowProducerListener.java
@@ -1,5 +1,7 @@
 package com.netflix.hollow.api.producer;
 
+import java.util.EventListener;
+
 /**
  * Beta API subject to change.
  *
@@ -7,67 +9,83 @@ package com.netflix.hollow.api.producer;
  *
  * @author Kinesh Satiya {@literal kineshsatiya@gmail.com}.
  */
-public interface HollowProducerListener {
+public interface HollowProducerListener extends EventListener {
 
     /**
-     * This method is called once {@code HollowProducer} has been initialized with the expected schemas.
-     */
-    public void onProducerInit();
-
-    /**
-     * This method is called once {@code HollowProducer} has restored from previously produced state.
-     * If previous state is not available or its the first state, then this callback will not be called.
+     * Called after the {@code HollowProducer} has initialized its data model.
      *
+     * @param producer the producer which initialized
+     */
+    public void onProducerInit(HollowProducer producer);
+
+    /**
+     * Called after the {@code HollowProducer} has restored its data state to the indicated version.
+     * If previous state is not available to restore from, then this callback will not be called.
+     *
+     * @param producer the producer which restored
      * @param restoreVersion Version from which the state for {@code HollowProducer} was restored.
      */
-    public void onProducerRestore(long restoreVersion);
+    public void onProducerRestore(HollowProducer producer, long restoreVersion);
 
     /**
-     * This method is called before starting to populating the data into {@code HollowWriteStateEngine}.
+     * Called when the {@code HollowProducer} has begun a new cycle.
      *
+     * @param producer the producer which started a cycle
      * @param version Version produced by the {@code HollowProducer} for new cycle about to start.
      */
-    public void onCycleStart(long version);
+    public void onCycleStart(HollowProducer producer, long version);
 
     /**
-     * This method is called when there is no change observed in write state engine, in the current cycle. This would also mean, no delta can be produced for publishing.
+     * Called after the new state has been populated if the {@code HollowProducer} detects that no data has changed, thus no snapshot nor delta should be produced.<p>
      *
+     * This is a terminal cycle stage; no other stages notifications will be sent for this cycle; the {@link #onCycleComplete(CycleStatus)} will be
+     * notified with @{code SUCCESS}.
+     *
+     * @param producer the producer
      * @param version Current version of the cycle.
      */
-    public void onNoDeltaAvailable(long version);
+    public void onNoDeltaAvailable(HollowProducer producer, long version);
 
     /**
-     * This method is called before starting to published the {@code HollowBlob} produced by the {@code HollowProducer}.
+     * Called when the {@code HollowProducer} has begun publishing the {@code HollowBlob} produced this cycle.
      *
+     * @param producer the producer beginning to publish
      * @param version Version to be published.
      */
-    public void onPublishStart(long version);
+    public void onPublishStart(HollowProducer producer, long version);
 
     /**
-     * This method is called upon successful publishing of the resulting {@code HollowBlob} produced by the {@code HollowProducer.}
+     * Called after the publish stage finishes normally or abnormally. On successful completion this indicates that
+     * the {@code HollowBlob} produced this cycle has been published to the blob store.
      *
-     * @param status CycleStatus of the publish stage.
+     * @param status CycleStatus of the publish stage. {@link com.netflix.hollow.api.producer.CycleStatus#getStatus()} will return {@code SUCCESS}
+     *   when the publish was successful; @{code FAIL} otherwise.
      */
     public void onPublishComplete(CycleStatus status);
 
     /**
-     * This method is called before announcing the availability of new {@code HollowBlob}.
+     * Called when the {@code HollowProducer} has begun announcing the {@code HollowBlob} published this cycle.
      *
-     * @param version of {@code HollowBlob} that was announced.
+     * @param producer the proucer beginning the announcement
+     * @param version of {@code HollowBlob} that will be announced.
      */
-    public void onAnnouncementStart(long version);
+    public void onAnnouncementStart(HollowProducer producer, long version);
 
     /**
-     * This method is called when availability of new {@code HollowBlob} is announced.
+     * Called after the announcement stage finishes normally or abnormally. On successful completion this indicates
+     * that the {@code HollowBlob} published this cycle has been announced to consumers.
      *
-     * @param status CycleStatus of the announcement stage.
+     * @param status CycleStatus of the announcement stage. {@link com.netflix.hollow.api.producer.CycleStatus#getStatus()} will return {@code SUCCESS}
+     *   when the announce was successful; @{code FAIL} otherwise.
      */
     public void onAnnouncementComplete(CycleStatus status);
 
     /**
-     * This method is called when {@code HollowProducer} has successfully completed a cycle.
+     * Called after {@code HollowProducer} has completed a cycle normally or abnormally.
      *
-     * @param status CycleStatus when cycle completed.
+     * @param status CycleStatus of this cycle. {@link com.netflix.hollow.api.producer.CycleStatus#getStatus()} will return {@code SUCCESS}
+     *   when the a new state has been announced to consumers or when there were no changes to the data; it will return @{code FAIL}
+     *   when any stage fails or any other failure occurs during cycle processing.
      */
     public void onCycleComplete(CycleStatus status);
 
