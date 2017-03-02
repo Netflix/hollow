4cce4f85b163c78b8a9fd5bc213e0f69066f45de
Tim Taylor <tim@toolbear.io>
2017-03-01 21:40:01 -0800

    provide a default publisher that publishes & announces to local FS

---
+++ hollow/src/main/java/com/netflix/hollow/api/producer/fs/HollowFilesystemAnnouncer.java
@@ -0,0 +1,69 @@
+/*
+ *
+ *  Copyright 2017 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.hollow.api.producer.fs;
+
+import static java.nio.file.Files.createDirectories;
+import static java.nio.file.Files.newBufferedWriter;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.DSYNC;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.netflix.hollow.api.producer.HollowProducer;
+
+public class HollowFilesystemAnnouncer implements HollowProducer.Announcer {
+    private final Path publishPath;
+    private final Path annnouncementPath;
+
+    public HollowFilesystemAnnouncer(String namespace) {
+        this(namespace, "announced.version");
+    }
+
+    public HollowFilesystemAnnouncer(String namespace, String announcementFilename) {
+        this(Paths.get(System.getProperty("java.io.tmpdir"), namespace, "published"),
+                announcementFilename);
+    }
+
+    public HollowFilesystemAnnouncer(Path publishDir, String announcementFilename) {
+        this.publishPath = publishDir;
+        annnouncementPath = publishPath.resolve(announcementFilename);
+    }
+
+    @Override
+    public void announce(long stateVersion) {
+        BufferedWriter writer = null;
+        try {
+            createDirectories(publishPath);
+            writer = newBufferedWriter(annnouncementPath, CREATE, WRITE, TRUNCATE_EXISTING, DSYNC);
+            writer.write(String.valueOf(stateVersion));
+        } catch(IOException ex) {
+            throw new RuntimeException("Unable to write to announcement file", ex);
+        } finally {
+            try {
+                if(writer != null) writer.close();
+            } catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
