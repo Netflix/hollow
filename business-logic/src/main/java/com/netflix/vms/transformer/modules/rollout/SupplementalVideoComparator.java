d91f882aa21d53eb9cedac1fe22b5fb1d10e35dc
Kinesh Satiya <ksatiya@netflix.com>
2017-06-12 11:55:33 -0700

    fix comparators for supplemental videos

--- business-logic/src/main/java/com/netflix/vms/transformer/modules/rollout/SupplementalVideoComparator.java
+++ business-logic/src/main/java/com/netflix/vms/transformer/modules/rollout/SupplementalVideoComparator.java
@@ -8,7 +8,7 @@ public class SupplementalVideoComparator implements Comparator<SupplementalVideo
 
     @Override
     public int compare(SupplementalVideo o1, SupplementalVideo o2) {
-        int x = o1.seasonNumber;
+        int x = o1.sequenceNumber;
         int y = o2.sequenceNumber;
         return (x < y) ? -1 : ((x == y) ? 0 : 1);
     }
