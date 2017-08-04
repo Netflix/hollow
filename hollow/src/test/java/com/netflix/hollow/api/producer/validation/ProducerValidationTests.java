816215359b0c55b5905a3c4b5a4cb5a558f6dd22
lkancode <lavanya65@yahoo.com>
2017-08-03 17:32:44 -0700

    Move to right package and unit tests.

---
+++ hollow/src/test/java/com/netflix/hollow/api/producer/validation/ProducerValidationTests.java
@@ -0,0 +1,100 @@
+/*
+ *
+ *  Copyright 2017 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.hollow.api.producer.validation;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.netflix.hollow.api.consumer.HollowConsumer;
+import com.netflix.hollow.api.consumer.InMemoryBlobStore;
+import com.netflix.hollow.api.producer.HollowProducer;
+import com.netflix.hollow.api.producer.HollowProducer.Populator;
+import com.netflix.hollow.api.producer.HollowProducer.Validator.ValidationException;
+import com.netflix.hollow.api.producer.HollowProducer.WriteState;
+import com.netflix.hollow.api.producer.fs.HollowInMemoryBlobStager;
+import com.netflix.hollow.core.write.objectmapper.HollowPrimaryKey;
+
+public class ProducerValidationTests {
+    private InMemoryBlobStore blobStore;
+    
+    @Before
+    public void setUp() {
+        blobStore = new InMemoryBlobStore();
+    }
+    
+    @Test
+    public void duplicateDetectionFailureTest() {
+        HollowProducer producer = HollowProducer.withPublisher(blobStore)
+                                                .withBlobStager(new HollowInMemoryBlobStager())
+                                                .withValidator(new DuplicateDataDetectionValidator("TypeWithPrimaryKey"))
+                                                .build();
+        
+        try {
+            //runCycle(producer, 1);
+        	producer.runCycle(new Populator() {
+				
+				public void populate(WriteState newState) throws Exception {
+					newState.add(new TypeWithPrimaryKey(1, "Brad Pitt", "klsdjfla;sdjkf"));
+					newState.add(new TypeWithPrimaryKey(1, "Angelina Jolie", "as;dlkfjasd;l"));
+					newState.add(new TypeWithPrimaryKey(1, "Brad Pitt", "as;dlkfjasd;l"));
+				}
+			});
+            Assert.fail();
+        } catch(ValidationException expected) {
+            Assert.assertEquals(1, expected.getIndividualFailures().size());
+            //System.out.println("Message: "+expected.getIndividualFailures().get(0).getMessage());
+            Assert.assertTrue(expected.getIndividualFailures().get(0).getMessage().startsWith("Duplicate keys found for type TypeWithPrimaryKey"));
+        }
+    }
+    
+    @Test
+    public void duplicateDetectionSuccessTest() {
+        HollowProducer producer = HollowProducer.withPublisher(blobStore)
+                                                .withBlobStager(new HollowInMemoryBlobStager())
+                                                .withValidator(new DuplicateDataDetectionValidator("TypeWithPrimaryKey"))
+                                                .build();
+        
+            //runCycle(producer, 1);
+        	producer.runCycle(new Populator() {
+				
+				public void populate(WriteState newState) throws Exception {
+					newState.add(new TypeWithPrimaryKey(1, "Brad Pitt", "klsdjfla;sdjkf"));
+					newState.add(new TypeWithPrimaryKey(1, "Angelina Jolie", "as;dlkfjasd;l"));
+				}
+			});
+        	
+        	HollowConsumer consumer = HollowConsumer.withBlobRetriever(blobStore).build();
+        	consumer.triggerRefresh();
+        	Assert.assertEquals(2, consumer.getStateEngine().getTypeState("TypeWithPrimaryKey").getPopulatedOrdinals().cardinality());
+    }
+    
+    
+    @HollowPrimaryKey(fields={"id","name"})
+    static class TypeWithPrimaryKey{
+    	int id;
+    	String name;
+    	String desc;
+    	
+    	TypeWithPrimaryKey(int id, String name, String desc){
+    		this.id=id;
+    		this.name=name;
+    		this.desc = desc;
+    	}
+    }
+}
