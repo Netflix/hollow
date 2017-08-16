f97a4a989827a67f2750698d7ea891c3ce232d1b
Drew Koszewnik <dkoszewnik@netflix.com>
2017-08-16 15:11:18 -0700

    Bugfix: HollowSchemaSorter and HollowCombiner now work even if referenced schemas/types are not loaded

--- hollow/src/main/java/com/netflix/hollow/tools/combine/HollowCombinerOrdinalRemapper.java
+++ hollow/src/main/java/com/netflix/hollow/tools/combine/HollowCombinerOrdinalRemapper.java
@@ -44,6 +44,9 @@ public class HollowCombinerOrdinalRemapper implements OrdinalRemapper {
     public int getMappedOrdinal(String type, int originalOrdinal) {
         int typeMapping[] = typeMappings.get(type);
         
+        if(typeMapping == null)
+            return originalOrdinal;
+        
         if(typeMapping[originalOrdinal] == -1)
             typeMapping[originalOrdinal] = combiner.copyOrdinal(type, originalOrdinal);
         
