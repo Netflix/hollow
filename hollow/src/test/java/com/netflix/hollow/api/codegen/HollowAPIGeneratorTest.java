eeab6630962e4c66f10f18147dac17c1959829a0
davesu <dave.su@yahoo.com>
2017-09-05 09:33:53 -0700

    clean up HollowAPIGeneratorTest

--- hollow/src/test/java/com/netflix/hollow/api/codegen/HollowAPIGeneratorTest.java
+++ hollow/src/test/java/com/netflix/hollow/api/codegen/HollowAPIGeneratorTest.java
@@ -3,18 +3,11 @@ package com.netflix.hollow.api.codegen;
 import com.netflix.hollow.core.write.HollowWriteStateEngine;
 import com.netflix.hollow.core.write.objectmapper.HollowObjectMapper;
 import java.io.File;
-import java.io.IOException;
-import org.junit.Before;
-import org.junit.Test;
 
 public class HollowAPIGeneratorTest {
-    private String baseDir = "/tmp"; //System.getProperty("java.io.tmpdir");
+    public static void main(String[] args) throws Exception {
+        String baseDir = System.getProperty("java.io.tmpdir");
 
-    @Before
-    public void setUp() throws IOException {}
-
-    @Test
-    public void testBooleanFieldErgonimics() throws Exception {
         // Setup
         String packageName = "booleanfieldergo";
         String srcDir = String.format("%s/%s/src/", baseDir, packageName);
