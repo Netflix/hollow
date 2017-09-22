90e193aff4b16f3d73ea466f59ea00d4b5d71808
Kinesh Satiya <ksatiya@netflix.com>
2017-09-22 11:46:19 -0400

    Update logging of input versions

--- server/src/main/java/com/netflix/vms/transformer/input/VMSInputDataVersionLogger.java
+++ server/src/main/java/com/netflix/vms/transformer/input/VMSInputDataVersionLogger.java
@@ -1,62 +1,47 @@
 package com.netflix.vms.transformer.input;
 
+import com.netflix.vms.logging.TaggingLogger;
 import static com.netflix.vms.transformer.common.io.TransformerLogTag.InputDataVersionIds;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 import java.util.TimeZone;
-import java.util.Objects;
-
-import com.netflix.vms.logging.TaggingLogger;
 
 public class VMSInputDataVersionLogger {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
 
 
     public static void logInputVersions(Map<String, String> inputBlobHeaders, TaggingLogger logger) {
-        for(Map.Entry<String, String> entry : inputBlobHeaders.entrySet()) {
-            if(entry.getKey().endsWith("_coldstart")) {
-                String mutationGroup = entry.getKey().substring(0, entry.getKey().indexOf("_coldstart"));
-                String latestColdstartVersion = entry.getValue();
-                String coldstartKeybase = inputBlobHeaders.get(mutationGroup + "_coldstartKeybase");
+        for (Map.Entry<String, String> entry : inputBlobHeaders.entrySet()) {
+            if (entry.getKey().endsWith("_coldstart")) {
 
-                // coldstart filename
-                String coldstartFilename = inputBlobHeaders.get(mutationGroup + "_coldstartFile") != null ? inputBlobHeaders.get(mutationGroup + "_coldstartFile") : "null";
+                String inputName = entry.getKey().substring(0, entry.getKey().indexOf("_coldstart"));
+                String latestCombinedVersion = entry.getValue();
 
-                // publish time in filestore
-                String formattedDate = "null";
-                if (inputBlobHeaders.get(mutationGroup + "_coldstartFilePublishTime") != null) {
-                    long publishTime = Long.valueOf(inputBlobHeaders.get(mutationGroup + "_coldstartFilePublishTime"));
+                String inputType = inputBlobHeaders.get(inputName + "coldstartType");
+                String coldstartKeybase = inputBlobHeaders.get(inputName + "_coldstartKeybase");
+                String coldstartFilename = inputBlobHeaders.get(inputName + "_coldstartFile") != null ? inputBlobHeaders.get(inputName + "_coldstartFile") : "null";
+                String formattedPublishDate = "n/a";
+                if (inputBlobHeaders.get(inputName + "_coldstartFilePublishTime") != null) {
+                    long publishTime = Long.valueOf(inputBlobHeaders.get(inputName + "_coldstartFilePublishTime"));
                     dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
                     Date publishDate = new Date(publishTime);
-                    formattedDate = dateFormat.format(publishDate);
+                    formattedPublishDate = dateFormat.format(publishDate);
                 }
+                String latestEventId = inputBlobHeaders.get(inputName + "_eventsCheckpoints");
+                String checkpoint = inputBlobHeaders.get(inputName + "_eventsLatest");
 
                 StringBuilder sb = new StringBuilder();
-                sb.append("mutationGroup=");
-                sb.append(mutationGroup);
-                sb.append(" coldstartVersionId=");
-                sb.append(latestColdstartVersion);
-                sb.append(" coldstartS3Filename=");
-                sb.append(coldstartFilename);
-                sb.append(" isColdstartPinned=false"); // FIXME: timt: what's this?
-                sb.append(" coldstartFilePublishDate=");
-                sb.append(formattedDate.toString());
-                for(String k : new String[]{
-                    "coldstartFile",
-                    "coldstartFilePublishTime",
-                    "coldstartKeybase",
-                    "eventsBackend",
-                    "eventsCheckpoints",
-                    "eventsLatest"
-                  }) {
-                  String v = inputBlobHeaders.get(mutationGroup + "_" + k);
-                  sb.append(' ');
-                  sb.append(k);
-                  sb.append('=');
-                  sb.append(Objects.toString(v, ""));
-                }
+                sb.append("Input=").append(inputName)
+                  .append(" Keybase=").append(coldstartKeybase)
+                  .append(" Type=").append(inputType)
+                  .append(" Version=").append(latestCombinedVersion)
+                  .append(" EventId=").append(latestEventId)
+                  .append(" EventCheckpoint=").append(checkpoint)
+                  .append(" FileName=").append(coldstartFilename)
+                  .append(" PublishTime=").append(formattedPublishDate);
+
                 logger.info(InputDataVersionIds, sb.toString());
             }
         }
