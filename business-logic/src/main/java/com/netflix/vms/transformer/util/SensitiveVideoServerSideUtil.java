6665ae1b47f24d957482ff1dbe02e055f23acb09
dsu <dsu@netflix.com>
2017-10-18 08:37:52 -0700

    Handle DVD+Original Title properly for metadataReleaseDate
    - Avoid leaking Original Title metadata even when there is Platic/DVD
    info since we no longer can assume that having DVD data means it is no
    longer sensitive

--- business-logic/src/main/java/com/netflix/vms/transformer/util/SensitiveVideoServerSideUtil.java
+++ business-logic/src/main/java/com/netflix/vms/transformer/util/SensitiveVideoServerSideUtil.java
@@ -37,9 +37,10 @@ public class SensitiveVideoServerSideUtil {
         return false;
     }
 
-    public static final Date getMetadataAvailabilityDate(boolean inDVDCatalog, Set<VideoSetType> videoSetTypes, Long firstDisplayDate, Long firstPhaseStartDate,
+    public static final Date getMetadataAvailabilityDate(boolean inDVDCatalog, boolean isOriginal, Set<VideoSetType> videoSetTypes, Long firstDisplayDate, Long firstPhaseStartDate,
             Long availabilityDate, Integer prePromoDays, Integer metadataReleaseDays, CycleConstants constants, Long earliestScheduledPhaseDate) {
-        if (inDVDCatalog) return constants.EXEMPT_HOLD_BACK_DATE;
+        if (inDVDCatalog && !isOriginal)
+            return constants.EXEMPT_HOLD_BACK_DATE;
 
         Long gracePeriodDate = null;
         if (availabilityDate!=null) {
