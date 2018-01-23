74c0145a72391d9744d40940c148be7f51195eaf
Kinesh Satiya <ksatiya@netflix.com>
2018-01-22 19:12:03 -0800

    update unit tests

--- server/src/test/java/com/netflix/vms/transformer/common/config/OctoberSkyDataTest.java
+++ server/src/test/java/com/netflix/vms/transformer/common/config/OctoberSkyDataTest.java
@@ -2,7 +2,9 @@ package com.netflix.vms.transformer.common.config;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 import com.netflix.cup.CupModule;
+import com.netflix.governator.InjectorBuilder;
 import com.netflix.governator.guice.test.ModulesForTesting;
 import com.netflix.governator.guice.test.junit4.GovernatorJunit4ClassRunner;
 import com.netflix.runtime.health.guice.HealthModule;
@@ -12,6 +14,7 @@ import com.netflix.vms.transformer.cup.CupLibraryImpl;
 import com.netflix.vms.transformer.health.TransformerServerHealthIndicator;
 import com.netflix.vms.transformer.octobersky.OctoberSkyDataImpl;
 import com.netflix.vms.transformer.startup.JerseyModule;
+import com.netflix.vms.transformer.startup.TransformerModule;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,37 +24,14 @@ import java.util.Set;
 /**
  * Test for reading multilingual countries
  */
-@RunWith(GovernatorJunit4ClassRunner.class)
-@ModulesForTesting({
-        OctoberSkyDataTest.TestOctoberSkyModule.class,
-})
 public class OctoberSkyDataTest {
-
-    @Inject
-    public OctoberSkyData octoberSkyData;
 
     @Test
     public void testGetMultiLingualCountries() {
+        Injector injector = InjectorBuilder.fromModules(new TransformerModule()).createInjector();
+        OctoberSkyData octoberSkyData = injector.getInstance(OctoberSkyDataImpl.class);
+
         Set<String> languages = octoberSkyData.getCatalogLanguages("BE");
         Assert.assertNotNull(languages);
-    }
-
-    public static class TestOctoberSkyModule extends AbstractModule {
-
-        @Override
-        public void configure() {
-            install(new RuntimeCoreModule());
-            install(new HealthModule() {
-                @Override
-                protected void configureHealth() {
-                    bindAdditionalHealthIndicator().to(TransformerServerHealthIndicator.class);
-                }
-            });
-            install(new JerseyModule());
-            install(new CupModule());
-
-            bind(OctoberSkyData.class).to(OctoberSkyDataImpl.class);
-            bind(CupLibrary.class).to(CupLibraryImpl.class);
-        }
     }
 }
