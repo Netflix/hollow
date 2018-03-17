3628c1a4f501df53b6bad3f050367cb8a89ade3f
Ammar Khaku <akhaku@netflix.com>
2018-03-16 18:50:24 -0700

    Ensure that circuit breaker job completes before delete job runs
    The circuit breaker job runs using files that the delete job
    deletes. Make sure it is complete before the delete job
    runs.
    Also add a little bit of documentation.

--- common-interfaces/src/main/java/com/netflix/vms/transformer/common/publish/workflow/PublicationJob.java
+++ common-interfaces/src/main/java/com/netflix/vms/transformer/common/publish/workflow/PublicationJob.java
@@ -1,26 +1,44 @@
 package com.netflix.vms.transformer.common.publish.workflow;
 
 public interface PublicationJob extends Runnable {
-    public static final long NOT_YET = Long.MIN_VALUE;
+    long NOT_YET = Long.MIN_VALUE;
 
     long getActualStartTimestamp();
 
+    /**
+     * If a job succeeded, returns the success timestamp, otherwise {@link NOT_YET}.
+     */
     long getCompletedTimestamp();
 
     long getCycleVersion();
 
     String getDisplayableStatus();
 
+    /**
+     * If a job failed, returns the fail timestamp, otherwise {@link NOT_YET}.
+     */
     long getFailedTimestamp();
 
+    /**
+     * If a job failed or succeeded, returns the timestamp it failed or succeeded, otherwise {@link NOT_YET}.
+     */
     long getFinishedTimestamp();
 
     String getJobName();
 
+    /**
+     * Returns true if the job has failed. Note that this returns false if the job succeeded.
+     */
     boolean hasJobFailed();
 
+    /**
+     * Returns true if the job has successfully completed. Note that this returns false if the job failed.
+     */
     boolean isComplete();
 
+    /**
+     * Returns true if the job is eligible for execution.
+     */
     boolean isEligible();
 
     void setJobQueue(PublicationJobQueue publicationJobQueue);
--- publish-workflow/src/main/java/com/netflix/vms/transformer/publish/workflow/job/HollowBlobDeleteFileJob.java
+++ publish-workflow/src/main/java/com/netflix/vms/transformer/publish/workflow/job/HollowBlobDeleteFileJob.java
@@ -8,36 +8,40 @@ import com.netflix.vms.transformer.publish.workflow.util.FileStatLogger;
 import java.io.File;
 import java.util.List;
 
+/**
+ * A job that deletes files from the local filesystem. This is generally used to delete
+ * hollow snapshot, delta, and reverse delta files stored on disk, in the java temporary
+ * folder.
+ */
 public class HollowBlobDeleteFileJob extends PublishWorkflowPublicationJob {
-    private final List<PublicationJob> publishJobs;
-    protected final String[] filesToDelete;
+    private final List<PublicationJob> circuitBreakerAndPublishJobs;
+    private final String[] filesToDelete;
 
-    public HollowBlobDeleteFileJob(PublishWorkflowContext ctx, List<PublicationJob> copyJobs, long version, String... filesToDelete) {
+    public HollowBlobDeleteFileJob(PublishWorkflowContext ctx,
+            List<PublicationJob> circuitBreakerAndPublishJobs, long version, String... filesToDelete) {
         super(ctx, "delete-files", version);
-        this.publishJobs = copyJobs;
+        this.circuitBreakerAndPublishJobs = circuitBreakerAndPublishJobs;
         this.filesToDelete = filesToDelete;
     }
 
     @Override
     protected boolean executeJob() {
-        for(String filename : filesToDelete) {
+        for (String filename : filesToDelete) {
             File f = new File(filename);
-            if(f.exists()) {
+            if (f.exists()) {
                 FileStatLogger.logFileState(ctx.getLogger(), TransformerLogTag.DeletedTmpFile, "cleanup File", f);
                 f.delete();
             }
         }
-
         return true;
     }
 
     @Override
     public boolean isEligible() {
-        for(PublicationJob job : publishJobs) {
-            if(!job.isComplete() && !job.hasJobFailed())
+        for (PublicationJob job : circuitBreakerAndPublishJobs) {
+            if (!job.isComplete() && !job.hasJobFailed())
                 return false;
         }
-
         return true;
     }
 
