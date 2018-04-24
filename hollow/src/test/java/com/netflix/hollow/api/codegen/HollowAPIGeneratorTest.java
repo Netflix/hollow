1af8565ebcc31f7176aeb01cc870eb805c78f58e
Roberto Perez Alcolea <roberto.perezalcolea@target.com>
2018-04-23 21:33:33 -0500

    fix imports in tests

--- hollow/src/test/java/com/netflix/hollow/api/codegen/HollowAPIGeneratorTest.java
+++ hollow/src/test/java/com/netflix/hollow/api/codegen/HollowAPIGeneratorTest.java
@@ -4,17 +4,18 @@ import com.netflix.hollow.core.write.HollowWriteStateEngine;
 import com.netflix.hollow.core.write.objectmapper.HollowObjectMapper;
 import java.io.File;
 import java.io.IOException;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.Test;
-
-
 import java.nio.file.Files;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
 
 public class HollowAPIGeneratorTest {
 
