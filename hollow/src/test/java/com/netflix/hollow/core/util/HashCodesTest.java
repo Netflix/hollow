8bede2d9539800af7d26fcb9ea2d93fcb3ff3e35
Tim Taylor <tim@toolbear.io>
2018-05-22 18:45:31 -0700

    avoid integer overflow in hashtable size calculation
    Previously overflowed when `numElements > Integer.MAX_VALUE / 10`.

--- hollow/src/test/java/com/netflix/hollow/core/util/HashCodesTest.java
+++ hollow/src/test/java/com/netflix/hollow/core/util/HashCodesTest.java
@@ -17,12 +17,16 @@
  */
 package com.netflix.hollow.core.util;
 
+import static com.netflix.hollow.core.HollowConstants.HASH_TABLE_MAX_BUCKETS;
+
 import com.netflix.hollow.core.memory.ByteDataBuffer;
 import com.netflix.hollow.core.memory.encoding.HashCodes;
 import com.netflix.hollow.core.memory.encoding.VarInt;
 import com.netflix.hollow.core.memory.pool.WastefulRecycler;
 import java.util.Random;
+import junit.framework.AssertionFailedError;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class HashCodesTest {
@@ -41,7 +45,82 @@ public class HashCodesTest {
             Assert.assertEquals(accurateStringHashCode(str), HashCodes.hashCode(str));
         }
     }
-    
+
+    @Test
+    public void testHashTableSize() {
+        // Current load factor is 10 / 7. If load factor calculation is changed, this test should be updated
+        int N;
+
+        try {
+            HashCodes.hashTableSize(-1);
+            Assert.fail("exception expected");
+        } catch (IllegalArgumentException ex) {
+            Assert.assertEquals("cannot be negative; numElements=-1", ex.getMessage());
+        }
+
+        Assert.assertEquals(1, HashCodes.hashTableSize(0));
+        Assert.assertEquals(2, HashCodes.hashTableSize(1));
+        Assert.assertEquals(4, HashCodes.hashTableSize(2));
+
+        // first integer overflow boundary condition (214_748_364)
+        N = Integer.MAX_VALUE / 10;
+        Assert.assertEquals(1 << 29, HashCodes.hashTableSize(N));
+        Assert.assertEquals(536870912, HashCodes.hashTableSize(N + 1));
+
+        // exceeding maximum hash table size (before load factor)
+        N = HASH_TABLE_MAX_BUCKETS;
+        Assert.assertEquals(1073741824, HashCodes.hashTableSize(N));
+        try {
+            HashCodes.hashTableSize(N + 1);
+            Assert.fail("exception expected");
+        } catch (IllegalArgumentException ex) {
+            Assert.assertEquals("exceeds maximum number of buckets; numElements=751619277", ex.getMessage());
+        }
+
+        // Note: technically these overflow conditions aren't reachable because max buckets is a lower
+        //       threshold. Keeping the assertions to avoid regressions.
+        N = (int)((1L<<31) * 7L / 10L);
+        try {
+            HashCodes.hashTableSize(N);
+            Assert.fail("exception expected");
+        } catch (IllegalArgumentException ex) {}
+        try {
+            HashCodes.hashTableSize(N + 1);
+            Assert.fail("exception expected");
+        } catch (IllegalArgumentException ex) {}
+
+        // max int
+        try {
+            HashCodes.hashTableSize(Integer.MAX_VALUE);
+            Assert.fail("exception expected");
+        } catch (IllegalArgumentException ex) {}
+    }
+
+    @Test
+    @Ignore
+    public void testHashTableSize_exhaustively() {
+        int size = HashCodes.hashTableSize(2);
+        for (int N=3; N<HASH_TABLE_MAX_BUCKETS; ++N) {
+            int s = HashCodes.hashTableSize(N);
+            if (s < size) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("expected size to grow or stay same; N=");
+                sb.append(N);
+                sb.append(" previous=");
+                sb.append(size);
+                sb.append("(~2^");
+                sb.append(31 - Integer.numberOfLeadingZeros(size));
+                sb.append(") size=");
+                sb.append(s);
+                sb.append("(~2^");
+                sb.append(31 - Integer.numberOfLeadingZeros(s));
+                sb.append(')');
+                throw new AssertionFailedError(sb.toString());
+            }
+            size = s;
+        }
+    }
+
     private String buildRandomString(boolean includeMultibyteCharacters, int strLen) {
         StringBuilder builder = new StringBuilder();
         
