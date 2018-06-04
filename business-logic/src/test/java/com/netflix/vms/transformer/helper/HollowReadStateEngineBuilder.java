fd3d96e587f4552598a52830402786d9ea0e45dc
Ammar Khaku <akhaku@netflix.com>
2018-06-04 10:10:04 -0700

    Use HollowReadStateEngineBuilder in unit tests
    Instead of re-implementing HollowReadStateEngineBuilder, use
    the version recently added to Hollow.
    Also bump Hollow to latest.release, instead of pinning to
    2.x.

---
+++ business-logic/src/test/java/com/netflix/vms/transformer/helper/HollowReadStateEngineBuilder.java
@@ -0,0 +1,20 @@
+package com.netflix.vms.transformer.helper;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
+
+/**
+ * Our version of a {@link com.netflix.hollow.test.HollowReadStateEngineBuilder}, which initializes
+ * our state engine with all types.
+ */
+public class HollowReadStateEngineBuilder extends
+        com.netflix.hollow.test.HollowReadStateEngineBuilder {
+    private static final Set<Class<?>> TOP_LEVEL_TYPES = new Reflections(
+            "com.netflix.vms.transformer.converterpojos").getSubTypesOf(Cloneable.class)
+            .stream().map(t -> (Class<?>) t).collect(Collectors.toSet());
+
+    public HollowReadStateEngineBuilder() {
+        super(TOP_LEVEL_TYPES);
+    }
+}
