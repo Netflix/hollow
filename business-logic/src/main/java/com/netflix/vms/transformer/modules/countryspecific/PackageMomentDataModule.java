9d9aa7cc1d060500490d350ee71671955f300870
Jatin Shah <jatins@netflix.com>
2018-10-30 13:05:37 -0700

    POI date from MPL

--- business-logic/src/main/java/com/netflix/vms/transformer/modules/countryspecific/PackageMomentDataModule.java
+++ business-logic/src/main/java/com/netflix/vms/transformer/modules/countryspecific/PackageMomentDataModule.java
@@ -1,6 +1,7 @@
 package com.netflix.vms.transformer.modules.countryspecific;
 
 import com.netflix.vms.transformer.common.TransformerContext;
+import com.netflix.vms.transformer.common.config.TransformerConfig;
 import com.netflix.vms.transformer.hollowinput.PackageHollow;
 import com.netflix.vms.transformer.hollowinput.TimecodeAnnotationHollow;
 import com.netflix.vms.transformer.hollowinput.TimecodeAnnotationsListHollow;
@@ -14,20 +15,53 @@ import java.util.Map;
 public class PackageMomentDataModule {
 
     private final Map<Integer, PackageMomentData> packageMomentDataByPackageId;
+    private final TransformerConfig config;
 
-    public PackageMomentDataModule() {
+    public PackageMomentDataModule(TransformerConfig config) {
         this.packageMomentDataByPackageId = new HashMap<>();
+        this.config = config;
     }
 
     public PackageMomentData getWindowPackageMomentData(PackageData packageData, PackageHollow inputPackage, TimecodeAnnotationHollow inputTimecodeAnnotation, TransformerContext ctx) {
+
         PackageMomentData packageMomentData = packageMomentDataByPackageId.get(Integer.valueOf(packageData.id));
         if (packageMomentData != null)
             return packageMomentData;
         
-        // Determine if timecode annotation is enabled
-        packageMomentData = createPackageMomentData(inputTimecodeAnnotation);        	
+        if (config.usePackagesFeedForTimecodes()) {
+            packageMomentData = createPackageMomentData(inputPackage);        	        	
+        } else {
+        	packageMomentData = createPackageMomentData(inputTimecodeAnnotation);
+        }
         packageMomentDataByPackageId.put(Integer.valueOf(packageData.id), packageMomentData);
         return packageMomentData;
+    }
+    
+    private PackageMomentData createPackageMomentData(PackageHollow inputPackage) {
+    	PackageMomentData data = new PackageMomentData();
+    	
+    	if(inputPackage != null) {
+    		TimecodeAnnotationsListHollow moments = inputPackage._getTimecodeAnnotations();
+    		
+    		if(moments != null) {
+    			for(TimecodedMomentAnnotationHollow moment : moments) {
+    				// If we find start or end moment, record that as well
+    				if(moment._getType()._getValue().equals("Start"))
+    					data.startMomentOffsetInMillis = moment._getStartMillis();
+    				if(moment._getType()._getValue().equals("Ending"))
+    					data.endMomentOffsetInMillis = moment._getStartMillis();
+    				    				
+        			TimecodeAnnotation annotation = new TimecodeAnnotation();
+        			annotation.type = moment._getType()._getValue().toCharArray();
+        			annotation.startMillis = moment._getStartMillis();
+        			annotation.endMillis = moment._getEndMillis();
+        			data.timecodes.add(annotation);    				
+    			}
+    		}
+
+    	}
+    	
+    	return data;
     }
     
     private PackageMomentData createPackageMomentData(TimecodeAnnotationHollow inputTimecodeAnnotation) {
