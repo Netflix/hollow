5d533ec50031b75561624ca7997dceba705f2f74
Ammar Khaku <akhaku@netflix.com>
2018-11-28 17:09:58 -0800

    Correctly calculate show window when rolling up window data

--- business-logic/src/main/java/com/netflix/vms/transformer/modules/countryspecific/DateWindowAggregator.java
+++ business-logic/src/main/java/com/netflix/vms/transformer/modules/countryspecific/DateWindowAggregator.java
@@ -2,6 +2,7 @@ package com.netflix.vms.transformer.modules.countryspecific;
 
 import com.netflix.vms.transformer.hollowoutput.DateWindow;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -13,27 +14,23 @@ public class DateWindowAggregator {
 
     private boolean merged = false;
 
-    public DateWindowAggregator() {
+    DateWindowAggregator() {
         this.windows = new HashSet<>();
-        this.mergedWindows = new ArrayList<DateWindow>();
+        this.mergedWindows = new ArrayList<>();
     }
 
-    public void addDateWindow(long startTime, long endTime) {
+    void addDateWindow(long startTime, long endTime) {
         windows.add(window(startTime, endTime));
     }
 
-    public void mergeDateWindows() {
+    void mergeDateWindows() {
         if (windows.size() == 0 || merged)
             return;
 
         List<DateWindow> sortedWindows = new ArrayList<>(windows);
 
-        sortedWindows.sort((o1, o2) -> {
-            int cmp = o1.startDateTimestamp < o2.startDateTimestamp ? -1 : (o1.startDateTimestamp == o2.startDateTimestamp ? 0 : 1);
-            if (cmp == 0)
-                cmp = o1.endDateTimestamp < o2.endDateTimestamp ? -1 : (o1.endDateTimestamp == o2.endDateTimestamp ? 0 : 1);
-            return cmp;
-        });
+        Comparator<DateWindow> c = Comparator.comparingLong(o1 -> o1.startDateTimestamp);
+        sortedWindows.sort(c.thenComparingLong(o1 -> o1.endDateTimestamp));
 
         long currentStartDate = sortedWindows.get(0).startDateTimestamp;
         long currentEndDate = sortedWindows.get(0).endDateTimestamp;
@@ -54,19 +51,26 @@ public class DateWindowAggregator {
         merged = true;
     }
 
-    public DateWindow matchDateWindowAgainstMergedDateWindows(long start, long end) {
-        for (int i = 0; i < mergedWindows.size(); i++) {
-            DateWindow window = mergedWindows.get(i);
-            if (window.endDateTimestamp >= start) {
-                if (window.startDateTimestamp >= start && window.endDateTimestamp <= end)
-                    return window;
-                start = Math.max(window.startDateTimestamp, start);
-                end = Math.min(window.endDateTimestamp, end);
-                return window(start, end);
-            }
-        }
-
-        return null;
+    /**
+     * Find the first window that is within {@code start} and {@code end}. That is, we're looking
+     * for the first window that starts after {@code start} and before {@code end}. We then bound
+     * that by the tighter of start/the actual window start, and end/the actual window end.
+     *
+     * Note that mergedWindows is already sorted in ascending order of startDateTimestamp.
+     *
+     * @return a DateWindow matching the above criteria, or null if none is found
+     */
+    DateWindow matchDateWindowAgainstMergedDateWindows(long start, long end) {
+        return mergedWindows.stream()
+                // windows that end after our start and start before our end
+                .filter(w -> start <= w.endDateTimestamp && end >= w.startDateTimestamp)
+                .map(window -> {
+                    boolean reuseExistingWindowObject =
+                            window.startDateTimestamp >= start && window.endDateTimestamp <= end;
+                    return reuseExistingWindowObject ? window
+                            : window(Math.max(window.startDateTimestamp, start),
+                                    Math.min(window.endDateTimestamp, end));
+                }).findFirst().orElse(null);
     }
 
     public void reset() {
