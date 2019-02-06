aa8c136c1b396430adf3d62c594dab4acff21b95
Paul Sandoz <psandoz@netflix.com>
2019-02-06 08:07:12 -0800

    Documentation POJO annotations.

--- hollow/src/main/java/com/netflix/hollow/core/write/objectmapper/HollowShardLargeType.java
+++ hollow/src/main/java/com/netflix/hollow/core/write/objectmapper/HollowShardLargeType.java
@@ -22,11 +22,47 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
+/**
+ * Indicates how many shards are used to encode hollow records, in effect
+ * partitioning records into {@code P} parts corresponding to the number of
+ * shards.
+ * <p>
+ * This annotation may be declared on a POJO or the field of a POJO that
+ * is of type {@code List}, {@code Set}, or {@code Map}.  It should be used
+ * when determining the number of shards automatically, given a target position
+ * size, is insufficient for a particular hollow record type.
+ * <p>
+ * If {@code N} records are encoded for a particular hollow record type,
+ * where each record has an unique ordinal {@code O},
+ * and where {@code 0 <= O < N}, and the records are encoded in {@code P}
+ * parts, then a record's assigned shard is the result of the expression
+ * {@code O & (P - 1)}.
+ * <p>
+ * If this annotation is absent then the number of shards is dynamically
+ * calculated given a target shard size in bytes
+ * (see
+ * {@link com.netflix.hollow.api.producer.HollowProducer.Builder#withTargetMaxTypeShardSize(long)
+ *  HollowProducer.Builder.withTargetMaxTypeShardSize}
+ * and
+ * {@link com.netflix.hollow.core.write.HollowWriteStateEngine#setTargetMaxTypeShardSize(long)
+ *  HollowWriteStateEngine.setTargetMaxTypeShardSize}
+ * )
+ * and the projected size in bytes of the hollow records for a particular type.
+ * For example, if the target shard size is set to a value in bytes of {@code 25MB} and the
+ * projected size of the hollow records for a type is {@code 50MB} then the number of shards
+ * will be {@code 2}.
+ * @see <a href="https://hollow.how/advanced-topics/#type-sharding">Type-sharding documentation</a>
+ */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.FIELD})
+@Target( {ElementType.TYPE, ElementType.FIELD})
 public @interface HollowShardLargeType {
-    
-    int numShards();
 
+    /**
+     * Returns the number of shards to partition a hollow record.
+     * <p>
+     * The number of shards must be a power of 2.
+     *
+     * @return the number of shards to partition a hollow record
+     */
+    int numShards();
 }
