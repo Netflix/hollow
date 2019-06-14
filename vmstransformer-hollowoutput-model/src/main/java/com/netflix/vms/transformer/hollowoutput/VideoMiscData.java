6dc472e78c5f83c45c4e9e561b69ee3e1a626dab
JP Leibundguth <jleibundguth@netflix.com>
2019-06-14 20:32:43 +0000

    Merge pull request #416 in VMS/vmstransformer from remove_icsmreview to master
    Squashed commit of the following:
    
    commit 5bc110089a2c81b69243b5456b9860a178ee3a2d
    Author: J.P. Leibundguth <jleibundguth@netflix.com>
    Date:   Fri Jun 14 10:40:31 2019 -0700
    
        step 4: remove CSMReview structures from transformer

--- vmstransformer-hollowoutput-model/src/main/java/com/netflix/vms/transformer/hollowoutput/VideoMiscData.java
+++ vmstransformer-hollowoutput-model/src/main/java/com/netflix/vms/transformer/hollowoutput/VideoMiscData.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class VideoMiscData implements Cloneable {
 
     public List<VideoAward> videoAwards = null;
-    public ICSMReview cSMReview = null;
 
     public boolean equals(Object other) {
         if(other == this)  return true;
@@ -16,23 +15,18 @@ public class VideoMiscData implements Cloneable {
         if(o.videoAwards == null) {
             if(videoAwards != null) return false;
         } else if(!o.videoAwards.equals(videoAwards)) return false;
-        if(o.cSMReview == null) {
-            if(cSMReview != null) return false;
-        } else if(!o.cSMReview.equals(cSMReview)) return false;
         return true;
     }
 
     public int hashCode() {
         int hashCode = 1;
         hashCode = hashCode * 31 + (videoAwards == null ? 1237 : videoAwards.hashCode());
-        hashCode = hashCode * 31 + (cSMReview == null ? 1237 : cSMReview.hashCode());
         return hashCode;
     }
 
     public String toString() {
         StringBuilder builder = new StringBuilder("VideoMiscData{");
         builder.append("videoAwards=").append(videoAwards);
-        builder.append(",cSMReview=").append(cSMReview);
         builder.append("}");
         return builder.toString();
     }
