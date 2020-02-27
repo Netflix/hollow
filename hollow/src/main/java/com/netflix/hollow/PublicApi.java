db01a2cc9c00d097dd402ce66ff4bf7f60c73d9d
Tim Taylor <timt@netflix.com>
2020-02-27 11:05:09 -0800

    add API contract tagging annotations
    Annotations to use on classes and/or methods to indicate our intended
    contract for API stability.
    
    Ideally all types with the `public` access modifier will be annotated.
    It may also useful to annotate package-private classes with
    `@Internal`; this clearly signals intent and eliminates the ambiguity
    of whether we "forgot" to make it public.
    
    Hollow supports Java 8 for the forseable future; so we cannot use
    the Java module system. Additionally Java modules don't offer the
    distinction that `@PublicApi` versus `@PublicSpi` do.

---
+++ hollow/src/main/java/com/netflix/hollow/PublicApi.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2016-2020 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.hollow;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This represents code that the hollow project considers public API and has an imperative to be stable within
+ * major releases.
+ *
+ * The guarantee  is for code calling classes and interfaces with this annotation, not derived from them.  New methods
+ * maybe be added which would break derivations but not callers.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = {CONSTRUCTOR, METHOD, TYPE})
+@Documented
+public @interface PublicApi {}
