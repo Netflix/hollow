2b5d33af55c5df7f5c59f0e7e1894b1124832b19
Tim Taylor <timt@netflix.com>
2020-02-27 11:05:09 -0800

    add API contract tagging annotations
    Annotations to use on classes and/or methods to indicate our intended
    contract for API stability.
    
    Ideally all types with the `public` access modifier will be annotated.
    It may also useful to annotate package-private classes with
    `@Internal`; this clearly signals intent and eliminates the ambiguity
    of whether we "forgot" to make it public.
    
    Hollow supports Java 8 for the forseable future; so we cannot use
    the Java module system. Additionally Java modules don't offer the
    distinction that `@PublicApi` versus `@PublicSpi` do.

---
+++ hollow/src/main/java/com/netflix/hollow/Internal.java
@@ -0,0 +1,36 @@
+/*
+ *  Copyright 2016-2020 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package com.netflix.hollow;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This represents code that the hollow project considers internal code that MAY not be stable within
+ * major releases.
+ *
+ * In general unnecessary changes will be avoided but you should not depend on internal classes being stable
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value={CONSTRUCTOR, METHOD, PACKAGE, TYPE})
+public @interface Internal {}
