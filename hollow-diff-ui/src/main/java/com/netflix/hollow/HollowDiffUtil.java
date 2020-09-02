86a71d2c997a63d8369c049cd952afc8a0633771
davesu <david.su@yahoo.com>
2020-09-01 19:05:20 -0700

    Support negative sizeInBytes

--- hollow-diff-ui/src/main/java/com/netflix/hollow/HollowDiffUtil.java
+++ hollow-diff-ui/src/main/java/com/netflix/hollow/HollowDiffUtil.java
@@ -4,9 +4,12 @@ import java.text.DecimalFormat;
 
 public class HollowDiffUtil {
     private static final String[] HEAP_SIZE_UNITS = new String[] { "B", "KB", "MB", "GB", "TB" };
+    private static final DecimalFormat BYTE_FORMATTER = new DecimalFormat("#,##0.#");
+
     public static String formatBytes(long sizeInBytes) {
-        if(sizeInBytes <= 0) return "0";
+        String sign = (sizeInBytes < 0) ? "-" : "";
+        sizeInBytes = Math.abs(sizeInBytes);
         int digitGroups = (int) (Math.log10(sizeInBytes)/Math.log10(1024));
-        return new DecimalFormat("#,##0.#").format(sizeInBytes/Math.pow(1024, digitGroups)) + " " + HEAP_SIZE_UNITS[digitGroups];
+        return sign + BYTE_FORMATTER.format(sizeInBytes/Math.pow(1024, digitGroups)) + " " + HEAP_SIZE_UNITS[digitGroups];
     }
 }
