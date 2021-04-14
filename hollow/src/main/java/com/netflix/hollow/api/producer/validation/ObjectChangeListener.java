cb76a46f60ed7180452d071b50c5acbc84f091d9
Sunjeet Singh <sunjeets@netflix.com>
2021-04-14 15:21:33 -0700

    iterate

--- hollow/src/main/java/com/netflix/hollow/api/producer/validation/ObjectChangeListener.java
+++ hollow/src/main/java/com/netflix/hollow/api/producer/validation/ObjectChangeListener.java
@@ -1,9 +1,10 @@
 package com.netflix.hollow.api.producer.validation;
 
+import com.netflix.hollow.api.objects.generic.GenericHollowObject;
 import com.netflix.hollow.api.producer.HollowProducer;
 import com.netflix.hollow.api.producer.listener.AnnouncementListener;
-import java.util.function.BiFunction;
-import java.util.function.Supplier;
+import com.netflix.hollow.core.read.dataaccess.HollowDataAccess;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 /**
@@ -24,10 +25,14 @@ import java.util.function.UnaryOperator;
 public class TypeSpecificObjectChangeListener implements AnnouncementListener {
 
     String typeName;
-    private final UnaryOperator<Object> callback;
+    private final Function<HollowDataAccess, A> apiFunction;
+    private final UnaryOperator<GenericHollowObject> callback;
 
-    public ObjectChangeListener(String typeName, UnaryOperator<Object> callback) {
+    public ObjectChangeListener(String typeName,
+            Function<HollowDataAccess, A> apiFunction,
+            UnaryOperator<GenericHollowObject> callback) {
         this.typeName = typeName;
+        this.apiFunction = apiFunction;
         this.callback = callback;
     }
 
@@ -35,9 +40,12 @@ public class TypeSpecificObjectChangeListener implements AnnouncementListener {
     public void onAnnouncementStart(HollowProducer.ReadState readState) {
         // compute +, -, * ordinals for type typeName
 
+        A hollowApi = apiFunction.apply(readState.getStateEngine());
         // for each added ordinal
-        // synthesize object by using typeName
-            // Object o = hollowApi.forType(typeName).getRecord(ordinal)
+            // synthesize object by using typeName
+            // GenericHollowObject object = hollowObjectFunction.apply(hollowApi, o);
+            // or
+            // GenericHollowObject object = new GenericHollowObject(object.getDataAccess(), TypeA.class.getSimpleName(), o);
             // callback.apply(o);
     }
 
