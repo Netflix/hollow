f370f79879826225972fdd921ee022795e22859e
davesu <david.su@yahoo.com>
2021-06-29 21:47:15 -0700

    unit test

--- hollow-ui-tools/src/test/java/com/netflix/hollow/ui/HollowDiffUtilTest.java
+++ hollow-ui-tools/src/test/java/com/netflix/hollow/ui/HollowDiffUtilTest.java
@@ -10,19 +10,19 @@ public class HollowDiffUtilTest {
     @Test
     public void testFormatBytes() {
         sampleTesting(1,"B", -10, 2, 0, 2, 10);
-        sampleTesting(Math.pow(2, 10),"KiB", -100, 50, 30, 100);
-        sampleTesting(Math.pow(2, 20),"MiB", -100, 50, 30, 100);
-        sampleTesting(Math.pow(2, 30),"GiB", -10, 30, 30, 100);
-        sampleTesting(Math.pow(2, 40),"TiB", -100, 50, 30, 100);
-        sampleTesting(Math.pow(2, 50),"PiB", -100, 50, 30, 100);
+        sampleTesting(Math.pow(2, 10),"KB", -100, 50, 30, 100);
+        sampleTesting(Math.pow(2, 20),"MB", -100, 50, 30, 100);
+        sampleTesting(Math.pow(2, 30),"GB", -10, 30, 30, 100);
+        sampleTesting(Math.pow(2, 40),"TB", -100, 50, 30, 100);
+        sampleTesting(Math.pow(2, 50),"PB", -100, 50, 30, 100);
 
         Assert.assertEquals( "-1,023 B", formatBytes(-1023));
-        Assert.assertEquals( "-1 KiB", formatBytes(-1024));
+        Assert.assertEquals( "-1 KB", formatBytes(-1024));
 
-        Assert.assertEquals( "1,000 TiB", formatBytes(1000 * (long)Math.pow(2, 40)));
-        Assert.assertEquals( "1 PiB", formatBytes(1024 * (long)Math.pow(2, 40)));
+        Assert.assertEquals( "1,000 TB", formatBytes(1000 * (long)Math.pow(2, 40)));
+        Assert.assertEquals( "1 PB", formatBytes(1024 * (long)Math.pow(2, 40)));
 
-        Assert.assertEquals( "8 EiB", formatBytes(Long.MAX_VALUE));
+        Assert.assertEquals( "8 EB", formatBytes(Long.MAX_VALUE));
     }
 
     private void sampleTesting(double multiple, String unit, long ... bytes) {
