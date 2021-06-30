ec6bd9078ae42b6f1f53deb2b41e28ea953819ac
davesu <david.su@yahoo.com>
2021-06-29 21:45:26 -0700

    DecimalFormat not ThreadSafe so could cause NPE

--- hollow-ui-tools/src/main/java/com/netflix/hollow/ui/HollowDiffUtil.java
+++ hollow-ui-tools/src/main/java/com/netflix/hollow/ui/HollowDiffUtil.java
@@ -3,16 +3,17 @@ package com.netflix.hollow.ui;
 import java.text.DecimalFormat;
 
 public class HollowDiffUtil {
-    private static final String[] HEAP_SIZE_UNITS = new String[] { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"};
-    private static final DecimalFormat BYTE_FORMATTER = new DecimalFormat("#,##0.#");
+    private static final String[] HEAP_SIZE_UNITS = new String[] { "B", "KB", "MB", "GB", "TB", "PB", "EB"};
 
     public static String formatBytes(long sizeInBytes) {
+
         if (sizeInBytes==0) return "0 B";
 
         String sign = (sizeInBytes < 0) ? "-" : "";
         sizeInBytes = Math.abs(sizeInBytes);
 
         int digitGroups = (int) (Math.log10(sizeInBytes)/Math.log10(1024));
+        DecimalFormat BYTE_FORMATTER = new DecimalFormat("#,##0.##");
         return sign + BYTE_FORMATTER.format(sizeInBytes / Math.pow(1024, digitGroups)) + " " + HEAP_SIZE_UNITS[digitGroups];
     }
 }
