1d09ae834b3d3cef39ba82745b68767443e2d4c3
stevenewald <stevenewald6@gmail.com>
2023-08-12 14:48:06 -0400

    Fixed bug in checking for unused ordinals

--- hollow/src/test/java/com/netflix/hollow/tools/util/ObjectInternPoolTest.java
+++ hollow/src/test/java/com/netflix/hollow/tools/util/ObjectInternPoolTest.java
@@ -38,6 +38,7 @@ public class ObjectInternPoolTest {
 
         int int1Ordinal = internPool.writeAndGetOrdinal(intObj1);
         int int2Ordinal = internPool.writeAndGetOrdinal(intObj2);
+        internPool.prepareForRead();
 
         int retrievedInt1 = (int) internPool.getObject(int1Ordinal, FieldType.INT);
 
@@ -49,6 +50,7 @@ public class ObjectInternPoolTest {
 
         int int3Ordinal = internPool.writeAndGetOrdinal(intObj3);
         int int4Ordinal = internPool.writeAndGetOrdinal(intObj4);
+        internPool.prepareForRead();
 
         int retrievedInt3 = (int) internPool.getObject(int3Ordinal, FieldType.INT);
 
@@ -65,6 +67,7 @@ public class ObjectInternPoolTest {
 
         int float1Ordinal = internPool.writeAndGetOrdinal(floatObj1);
         int float2Ordinal = internPool.writeAndGetOrdinal(floatObj2);
+        internPool.prepareForRead();
 
         float retrievedFloat1 = (float) internPool.getObject(float1Ordinal, FieldType.FLOAT);
 
@@ -73,6 +76,7 @@ public class ObjectInternPoolTest {
 
         int float3Ordinal = internPool.writeAndGetOrdinal(floatObj3);
         int float4Ordinal = internPool.writeAndGetOrdinal(floatObj4);
+        internPool.prepareForRead();
 
         float retrievedFloat2 = (float) internPool.getObject(float3Ordinal, FieldType.FLOAT);
 
@@ -89,6 +93,7 @@ public class ObjectInternPoolTest {
 
         int long1Ordinal = internPool.writeAndGetOrdinal(longObj1);
         int long2Ordinal = internPool.writeAndGetOrdinal(longObj2);
+        internPool.prepareForRead();
 
         long retrievedLong1 = (Long) internPool.getObject(long1Ordinal, FieldType.LONG);
 
@@ -97,6 +102,7 @@ public class ObjectInternPoolTest {
 
         int long3Ordinal = internPool.writeAndGetOrdinal(longObj3);
         int long4Ordinal = internPool.writeAndGetOrdinal(longObj4);
+        internPool.prepareForRead();
 
         long retrievedLong2 = (Long) internPool.getObject(long3Ordinal, FieldType.LONG);
 
@@ -113,6 +119,7 @@ public class ObjectInternPoolTest {
 
         int double1Ordinal = internPool.writeAndGetOrdinal(doubleObj1);
         int double2Ordinal = internPool.writeAndGetOrdinal(doubleObj2);
+        internPool.prepareForRead();
 
         double retrievedDouble1 = (double) internPool.getObject(double1Ordinal, FieldType.DOUBLE);
 
@@ -121,6 +128,7 @@ public class ObjectInternPoolTest {
 
         int double3Ordinal = internPool.writeAndGetOrdinal(doubleObj3);
         int double4Ordinal = internPool.writeAndGetOrdinal(doubleObj4);
+        internPool.prepareForRead();
 
         double retrievedDouble2 = (double) internPool.getObject(double3Ordinal, FieldType.DOUBLE);
 
@@ -137,6 +145,7 @@ public class ObjectInternPoolTest {
 
         int string1Ordinal = internPool.writeAndGetOrdinal(stringObj1);
         int string2Ordinal = internPool.writeAndGetOrdinal(stringObj2);
+        internPool.prepareForRead();
 
         String retrievedString1 = (String) internPool.getObject(string1Ordinal, FieldType.STRING);
 
@@ -145,6 +154,7 @@ public class ObjectInternPoolTest {
 
         int string3Ordinal = internPool.writeAndGetOrdinal(stringObj3);
         int string4Ordinal = internPool.writeAndGetOrdinal(stringObj4);
+        internPool.prepareForRead();
 
         String retrievedString2 = (String) internPool.getObject(string3Ordinal, FieldType.STRING);
 
@@ -161,6 +171,7 @@ public class ObjectInternPoolTest {
 
         int bool1Ordinal = internPool.writeAndGetOrdinal(boolObj1);
         int bool2Ordinal = internPool.writeAndGetOrdinal(boolObj2);
+        internPool.prepareForRead();
 
         boolean retrievedBool1 = (boolean) internPool.getObject(bool1Ordinal, FieldType.BOOLEAN);
 
@@ -169,6 +180,7 @@ public class ObjectInternPoolTest {
 
         int bool3Ordinal = internPool.writeAndGetOrdinal(boolObj3);
         int bool4Ordinal = internPool.writeAndGetOrdinal(boolObj4);
+        internPool.prepareForRead();
 
         boolean retrievedBool2 = (boolean) internPool.getObject(bool3Ordinal, FieldType.BOOLEAN);
 
