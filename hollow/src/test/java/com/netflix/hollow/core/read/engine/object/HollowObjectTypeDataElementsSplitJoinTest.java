7269186758b6148770528aff3a5caaa820dea390
Sunjeet Singh <sunjeets@netflix.com>
2023-10-03 08:10:49 -0700

    SplitJoin tests

--- hollow/src/test/java/com/netflix/hollow/core/read/engine/object/HollowObjectTypeDataElementsSplitJoinTest.java
+++ hollow/src/test/java/com/netflix/hollow/core/read/engine/object/HollowObjectTypeDataElementsSplitJoinTest.java
@@ -2,9 +2,15 @@ package com.netflix.hollow.core.read.engine.object;
 
 import static org.junit.Assert.assertEquals;
 
+import com.netflix.hollow.api.consumer.HollowConsumer;
+import com.netflix.hollow.api.consumer.fs.HollowFilesystemBlobRetriever;
+import com.netflix.hollow.core.read.engine.HollowReadStateEngine;
+import com.netflix.hollow.core.schema.HollowSchema;
 import com.netflix.hollow.core.write.HollowObjectTypeWriteState;
 import com.netflix.hollow.tools.checksum.HollowChecksum;
 import java.io.IOException;
+import java.nio.file.Paths;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HollowObjectTypeDataElementsSplitJoinTest extends AbstractHollowObjectTypeDataElementsSplitJoinTest {
@@ -36,5 +42,40 @@ public class HollowObjectTypeDataElementsSplitJoinTest extends AbstractHollowObj
                 assertEquals(origChecksum, resultChecksum);
             }
         }
+    }
+
+    // manually invoked
+    // @Test
+    public void testSplittingAndJoiningWithVms() throws Exception {
+
+        String blobPath = null; // dir where snapshot blob exists for e.g. "/tmp/";
+        long v = 0l; // snapshot version for e.g. 20230915162636001l;
+        String objectTypeWithOneShard = null; // type name corresponding to an Object type with single shard for e.g. "Movie";
+        int numSplits = 2;
+
+        if (blobPath==null || v==0l || objectTypeWithOneShard==null) {
+            throw new IllegalArgumentException("These arguments need to be specified");
+        }
+        HollowFilesystemBlobRetriever hollowBlobRetriever = new HollowFilesystemBlobRetriever(Paths.get(blobPath));
+        HollowConsumer c = HollowConsumer.withBlobRetriever(hollowBlobRetriever).build();
+        c.triggerRefreshTo(v);
+        HollowReadStateEngine readStateEngine = c.getStateEngine();
+
+        HollowObjectTypeReadState typeState = (HollowObjectTypeReadState) readStateEngine.getTypeState(objectTypeWithOneShard);
+        HollowSchema origSchema = typeState.getSchema();
+        HollowChecksum originalChecksum = typeState.getChecksum(origSchema);
+
+        assertEquals(1, typeState.numShards());
+
+        HollowObjectTypeDataElementsSplitter splitter = new HollowObjectTypeDataElementsSplitter();
+        HollowObjectTypeDataElements[] splitElements = splitter.split(typeState.currentDataElements()[0], numSplits);
+
+        HollowObjectTypeDataElementsJoiner joiner = new HollowObjectTypeDataElementsJoiner();
+        HollowObjectTypeDataElements joinedElements = joiner.join(splitElements);
+
+        typeState.setCurrentData(joinedElements);
+        HollowChecksum newChecksum = typeState.getChecksum(origSchema);
+
+        Assert.assertEquals(originalChecksum, newChecksum);
     }
 }
--- hollow/src/test/java/com/netflix/hollow/core/read/object/HollowObjectShardedTest.java
+++ hollow/src/test/java/com/netflix/hollow/core/read/object/HollowObjectShardedTest.java
@@ -103,6 +103,4 @@ public class HollowObjectShardedTest extends AbstractStateEngineTest {
     }
 
 
-
-
 }
