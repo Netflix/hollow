34d9a21e014a3e0cc3a76ca9ea80355fe4411e28
Sunjeet Singh <sunjeets@netflix.com>
2023-10-09 07:53:54 -0700

    Restore private modifier, cleanup, more test

--- hollow/src/test/java/com/netflix/hollow/core/read/engine/object/HollowObjectTypeDataElementsJoinerTest.java
+++ hollow/src/test/java/com/netflix/hollow/core/read/engine/object/HollowObjectTypeDataElementsJoinerTest.java
@@ -2,20 +2,31 @@ package com.netflix.hollow.core.read.engine.object;
 
 import static org.junit.Assert.assertEquals;
 
+import com.netflix.hollow.core.write.HollowObjectTypeWriteState;
 import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class HollowObjectTypeDataElementsJoinerTest extends AbstractHollowObjectTypeDataElementsSplitJoinTest {
+    @Override
+    protected void initializeTypeStates() {
+        writeStateEngine.setTargetMaxTypeShardSize(16);
+        writeStateEngine.addTypeState(new HollowObjectTypeWriteState(schema));
+    }
+
     @Test
     public void testJoin() throws IOException {
         HollowObjectTypeDataElementsJoiner joiner = new HollowObjectTypeDataElementsJoiner();
 
-        HollowObjectTypeReadState typeReadState = populateTypeStateWith(5);
+        HollowObjectTypeReadState typeReadState = populateTypeStateWith(1);
         assertEquals(1, typeReadState.numShards());
-        assertDataUnchanged(5);
 
-        HollowObjectTypeDataElements joinedDataElements = joiner.join(typeReadState.currentDataElements());
+        HollowObjectTypeReadState typeReadStateSharded = populateTypeStateWith(5);
+        assertDataUnchanged(5);
+        assertEquals(8, typeReadStateSharded.numShards());
+
+        HollowObjectTypeDataElements joinedDataElements = joiner.join(typeReadStateSharded.currentDataElements());
+
         typeReadState.setCurrentData(joinedDataElements);
         assertDataUnchanged(5);
 
