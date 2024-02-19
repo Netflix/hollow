708fce31eebe223980941535a23a8b5a872b4609
piotr.auguscik <piotr.auguscik@codewise.com>
2024-02-19 17:15:48 +0100

    Add support for byte arrays in primary key of history

--- hollow/src/main/java/com/netflix/hollow/tools/util/ObjectInternPool.java
+++ hollow/src/main/java/com/netflix/hollow/tools/util/ObjectInternPool.java
@@ -49,6 +49,8 @@ public class ObjectInternPool {
                 return getLong(pointer);
             case STRING:
                 return getString(pointer);
+            case BYTES:
+                return getBytes(pointer);
             default:
                 throw new IllegalArgumentException("Unknown type " + type);
         }
@@ -82,13 +84,17 @@ public class ObjectInternPool {
     }
 
     public String getString(long pointer) {
+        return new String(getBytes(pointer));
+    }
+
+    public byte[] getBytes(long pointer) {
         ByteData byteData = ordinalMap.getByteData().getUnderlyingArray();
         int length = VarInt.readVInt(byteData, pointer);
         byte[] bytes = new byte[length];
         for(int i=0;i<length;i++) {
             bytes[i] = byteData.get(pointer+1+i);
         }
-        return new String(bytes);
+        return bytes;
     }
 
     public int writeAndGetOrdinal(Object objectToIntern) {
