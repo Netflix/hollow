d0bf152970b814f94163c163acc34a508b00c436
Ao Zeng <azeng@netflix.com>
2026-03-09 20:11:30 -0700

    change list test so it fails without the fix;

--- hollow/src/test/java/com/netflix/hollow/core/read/engine/list/HollowListTypeReadStateTest.java
+++ hollow/src/test/java/com/netflix/hollow/core/read/engine/list/HollowListTypeReadStateTest.java
@@ -19,19 +19,19 @@ public class HollowListTypeReadStateTest extends AbstractHollowListTypeDataEleme
         writeStateEngine.addTypeState(new HollowListTypeWriteState(listSchema, 2));
 
         // Cycle 1
-        // populate TestObject with ordinal values 0 - 6
-        populateWriteStateEngine(writeStateEngine, schema, 6);
-        // populate TestList with ordinal values 0 - 6
+        // populate TestObject with ordinal values 0 - 7
+        populateWriteStateEngine(writeStateEngine, schema, 8);
+        // populate TestList with ordinal values 0 - 7
         populateWriteStateEngineWithListRecords(new int[][]{
-                {0}, {1}, {2}, {3}, {4}, {5}
+                {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}
         });
         roundTripSnapshot();
 
         // Cycle 2
         // readded all the TestObject records.
         // for TestList:
-        // Shard 0 (even: 0=hole, 2=pop, 4=pop) -> 1 hole
-        // Shard 1 (odd: 1=hole, 3=hole, 5=hole) -> 3 holes
+        // Shard 0 (even: 0=hole, 2=pop, 4=pop, 6=hole) -> 2 hole
+        // Shard 1 (odd: 1=hole, 3=hole, 5=hole, 7=hole) -> 4 holes
         populateWriteStateEngine(writeStateEngine, schema, 6);
         populateWriteStateEngineWithListRecords(new int[][]{
                 {2}, {4}
@@ -42,8 +42,8 @@ public class HollowListTypeReadStateTest extends AbstractHollowListTypeDataEleme
         assertEquals(2, typeState.numShards());
         assertEquals(2, typeState.getPopulatedOrdinals().cardinality());
         HollowListTypeReadStateShard[] shards = (HollowListTypeReadStateShard[])typeState.getShardsVolatile().getShards();
-        long holeCost = shards[0].dataElements.bitsPerListPointer/8L +
-                shards[1].dataElements.bitsPerListPointer*3L/8;
+        long holeCost = shards[0].dataElements.bitsPerListPointer*2L/8 +
+                shards[1].dataElements.bitsPerListPointer*4L/8;
         assertEquals(holeCost, typeState.getApproximateHoleCostInBytes());
     }
 
